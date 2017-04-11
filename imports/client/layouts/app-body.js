c0ea723ba614e5554648021014b8241d5dd4fb63
droka <daniel.roka@tutanota.com>
2017-04-11 23:22:06 +0200

    Cleaning unneeded imports

--- imports/client/layouts/app-body.js
+++ imports/client/layouts/app-body.js
@@ -4,18 +4,6 @@ import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { Template } from 'meteor/templating';
-import { ActiveRoute } from 'meteor/zimme:active-route';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { TAPi18n } from 'meteor/tap:i18n';
-import { T9n } from 'meteor/softwarerero:accounts-t9n';
-import { _ } from 'meteor/underscore';
-import { $ } from 'meteor/jquery';
-
-import { Topics } from '../../api/topics/topics.js';
-import { insert as insertTopic } from '../../api/topics/methods.js';
-import { Communities } from '../../api/communities/communities.js';
-import { Memberships } from '../../api/memberships/memberships.js';
-import { insert as insertMembership } from '../../api/memberships/methods.js';
 
 import '../components/loading.js';
 import '../components/side-panel.js';
