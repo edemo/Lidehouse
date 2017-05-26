b1a1fb5f8e81ba1c63a9e1d2058ebc1371f551d4
droka <daniel.roka@tutanota.com>
2017-05-26 14:45:05 +0200

    Fixing the Forum accordions on board page (was not functioning)

--- imports/ui_2/pages/board.js
+++ imports/ui_2/pages/board.js
@@ -1,3 +1,8 @@
+/* globals document */
+
+import { Template } from 'meteor/templating';
+
+import '../components/comments-section.html';
 import './board.html';
 
 Template.Board.onRendered(function boardOnRendered() {
@@ -20,7 +25,9 @@ Template.Board.onRendered(function boardOnRendered() {
       acc[i].classList.toggle("active");
       var panel = acc[i].nextElementSibling;
       panel.style.maxHeight = "none";
+    }
 
+    for (i = 0; i < acc2.length; i++) {
       acc2[i].onclick = function() {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
