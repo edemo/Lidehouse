7eb91050743aade1cb830cd537c35fa53b5373ac
droka <daniel.roka@tutanota.com>
2017-06-19 11:13:36 +0200

    Fixing choice order in the preferential vote UI

--- imports/ui_2/components/votebox.js
+++ imports/ui_2/components/votebox.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { moment } from 'meteor/momentjs:moment';
 import { TimeSync } from 'meteor/mizzao:timesync';
 import { displayError, displayMessage } from '/imports/ui/lib/errors.js';
@@ -10,7 +11,13 @@ import { $ } from 'meteor/jquery';
 import '../components/votebox.html';
 import '../components/comments-section.js';
 
+Template.Votebox.onCreated(function voteboxOnCreated() {
+  this.state = new ReactiveDict();
+  this.state.set('voteFinalized', false);
+});
+
 Template.Votebox.onRendered(function voteboxOnRendered() {
+  $('.sortable').sortable();
 });
 
 Template.Votebox.helpers({
@@ -31,11 +38,25 @@ Template.Votebox.helpers({
   comments() {
     return Comments.find({ topicId: this._id }, { sort: { createdAt: 1 } });
   },
+  // Single choice voting
   pressedClass(choice) {
     const activeMembershipId = Session.get('activeMembershipId');
     if (!activeMembershipId || !this.voteResults) return '';
     const ownVote = this.voteResults[activeMembershipId] && this.voteResults[activeMembershipId][0];
     return (ownVote === choice) && 'btn-pressed';
+  },
+  // Preferential voting
+  selectedChoices() {
+    const results = this.voteResults[Meteor.userId()];
+    if (results && results.length > 0) return results;
+    return this.vote.choices;
+  },
+  voteIsFinalized() {
+    return Template.instance().state.get('voteFinalized');
+  },
+  pressedClassForPreferential() {
+    if (Template.instance().state.get('voteFinalized')) return 'btn-pressed';
+    return '';
   },
 });
 
@@ -54,16 +75,24 @@ Template.Votebox.events({
     });
   },
   // event handler for the preferential vote type
-  'click .btn-votesend'(event) {
+  'click .btn-votesend'(event, instance) {
     const membershipId = Session.get('activeMembershipId');
     const topicId = this._id;
-    const choices = []; // TODO: Get the ordering and put it into the choices array
-    castVote.call({ topicId, membershipId, castedVote: choices }, function handle(err) {
-      if (err) {
-        displayError(err);
-        return;
-      }
-      displayMessage('success', 'Vote casted');
-    });
+    const voteFinalized = instance.state.get('voteFinalized');
+    if (!voteFinalized) {
+      const choices = []; // TODO: Get the ordering and put it into the choices array
+      castVote.call({ topicId, membershipId, castedVote: choices }, function handle(err) {
+        if (err) {
+          displayError(err);
+          return;
+        }
+        displayMessage('success', 'Vote casted');
+        instance.state.set('voteFinalized', true);
+        $('.sortable').sortable('disable');
+      });
+    } else { // voteFinalized === true
+      instance.state.set('voteFinalized', false);
+      $('.sortable').sortable('enable');
+    }
   },
 });
