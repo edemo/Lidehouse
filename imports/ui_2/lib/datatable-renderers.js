8f77772fe9dbaf6c3fd896d2115d640cad309a9e
droka <daniel.roka@tutanota.com>
2017-07-23 19:36:40 +0200

    Title instructions for the rendered buttons in datatables

--- imports/ui_2/lib/datatable-renderers.js
+++ imports/ui_2/lib/datatable-renderers.js
@@ -12,19 +12,19 @@ export const Render = {
     return moment(cellData).format('YYYY.MM.DD hh:mm');
   },
   buttonView(cellData, renderType, currentRow) {
-    const html = `<span data-id=${cellData} class="js-view nav-item glyphicon glyphicon-eye-open"></span>`;
+    const html = `<span data-id=${cellData} title=${__('view')} class="js-view nav-item glyphicon glyphicon-eye-open"></span>`;
     return html;
   },
   buttonEdit(cellData, renderType, currentRow) {
-    const html = `<span data-id=${cellData} class="js-edit nav-item icon-edit"></span>`;
+    const html = `<span data-id=${cellData} title=${__('edit')} class="js-edit nav-item icon-edit"></span>`;
     return html;
   },
   buttonDelete(cellData, renderType, currentRow) {
-    const html = `<span data-id=${cellData} class="js-delete nav-item icon-trash"></span>`;
+    const html = `<span data-id=${cellData} title=${__('delete')} class="js-delete nav-item icon-trash"></span>`;
     return html;
   },
   buttonRemove(cellData, renderType, currentRow) {
-    const html = `<span data-id=${cellData} class="js-remove glyphicon glyphicon-remove"></span>`;
+    const html = `<span data-id=${cellData} title=${__('remove')} class="js-remove glyphicon glyphicon-remove"></span>`;
     return html;
   },
   buttonJoin(cellData, renderType, currentRow) {
@@ -33,7 +33,7 @@ export const Render = {
   },
   buttonAssignParcelOwner(cellData, renderType, currentRow) {
     let html = `<a href=${FlowRouter.path('Parcel.owners', { _pid: cellData })}>`;
-    html += `<span data-id=${cellData} class="js-assign nav-item glyphicon glyphicon-user"></span>`;
+    html += `<span data-id=${cellData} title=${__('assign')} class="js-assign nav-item glyphicon glyphicon-user"></span>`;
     html += `</a>`;
     return html;
   },
