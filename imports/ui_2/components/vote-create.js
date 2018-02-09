8d899d494aef84a468c6be828c29f44d7003c00f
droka <daniel.roka@tutanota.com>
2018-02-09 17:06:13 +0100

    Choices section appears when needed

--- imports/ui_2/components/vote-create.js
+++ imports/ui_2/components/vote-create.js
@@ -4,6 +4,7 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { AutoForm } from 'meteor/aldeed:autoform';
+import { debugAssert } from '/imports/utils/assert.js';
 import { Topics } from '/imports/api/topics/topics.js';
 import { votingsExtensionSchema } from '/imports/api/topics/votings/votings.js';
 import './vote-create.html';
@@ -19,18 +20,29 @@ Template.Vote_create.helpers({
     schema.i18n('schemaVotings');
     return schema;
   },
+  needsChoicesSpecified() {
+    const voteType = AutoForm.getFieldValue('vote.type');
+    switch (voteType) {
+      case undefined: return false;
+      case 'petition': return false;
+      case 'yesno': return false;
+      case 'choose': return true;
+      case 'preferential': return true;
+      default: debugAssert(false); return false;
+    }
+  },
 });
 
 Template.Vote_create.onCreated(function voteCreateOnCreated() {
 });
-
+/*
 Template.Vote_create.events({
   'click .js-answer, click .js-add-answer'(event) {
     $('.choice-form')[0].classList.toggle('hidden');
     $('.js-answer')[0].classList.toggle('hidden');
   },
 });
-
+*/
 AutoForm.addModalHooks('af.voting.insert');
 AutoForm.addHooks('af.voting.insert', {
   formToDoc(doc) {
--- imports/ui_2/helpers/index.js
+++ imports/ui_2/helpers/index.js
@@ -2,3 +2,4 @@ import './utils.js';
 import './underscore.js';
 import './session.js';
 import './permissions.js';
+import './autoform.js';
