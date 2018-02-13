21aa1c194af00a2f2aef5d2b4d404b905c2ff891
droka <daniel.roka@tutanota.com>
2018-02-13 14:58:55 -0600

    Showing choices based on vote type selection

--- imports/ui_2/components/vote-create.js
+++ imports/ui_2/components/vote-create.js
@@ -1,6 +1,8 @@
 import { $ } from 'meteor/jquery';
 import { Meteor } from 'meteor/meteor';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Tracker } from 'meteor/tracker';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { AutoForm } from 'meteor/aldeed:autoform';
@@ -8,6 +10,24 @@ import { debugAssert } from '/imports/utils/assert.js';
 import { Topics } from '/imports/api/topics/topics.js';
 import { votingsExtensionSchema } from '/imports/api/topics/votings/votings.js';
 import './vote-create.html';
+
+let afVotingInsertInstance;
+
+Template.Vote_create.onCreated(function () {
+  const instance = Template.instance();
+  instance.choices = new ReactiveVar([]);
+  this.autorun(() => {
+    const currentVoteType = AutoForm.getFieldValue('vote.type', 'af.voting.insert');
+//    Tracker.nonreactive(() => {
+    const newChoices = Topics.voteTypeChoices[currentVoteType] || [];
+    instance.choices.set(newChoices);
+//    });
+  });
+});
+
+Template.Vote_create.onRendered(function () {
+  afVotingInsertInstance = Template.instance();
+});
 
 Template.Vote_create.helpers({
   collection() {
@@ -21,34 +41,45 @@ Template.Vote_create.helpers({
     return schema;
   },
   needsChoicesSpecified() {
-    const voteType = AutoForm.getFieldValue('vote.type');
-    switch (voteType) {
-      case undefined: return false;
-      case 'petition': return false;
-      case 'yesno': return false;
-      case 'choose': return true;
-      case 'preferential': return true;
-      default: debugAssert(false); return false;
-    }
+    const currentVoteType = AutoForm.getFieldValue('vote.type');
+    return !!(Topics.voteTypeChoices[currentVoteType]);
+  },
+  choices() {
+    const instance = Template.instance();
+    return instance.choices.get();
   },
 });
 
-Template.Vote_create.onCreated(function voteCreateOnCreated() {
-});
-/*
 Template.Vote_create.events({
-  'click .js-answer, click .js-add-answer'(event) {
-    $('.choice-form')[0].classList.toggle('hidden');
-    $('.js-answer')[0].classList.toggle('hidden');
+  'click .js-remove-choice'(event) {
+    const currentChoices = Template.instance().choices.get();
+    const removeIndex = $(event.target).data('index');
+    currentChoices.splice(removeIndex, 1);
+    Template.instance().choices.set(currentChoices);
+ //   Session.set('newVotingChoices', currentChoices);
+  },
+  'click .js-enter-choice'(event) {
+    let currentChoices = Template.instance().choices.get();
+    const newChoice = $('.editing input')[0].value;
+    currentChoices = currentChoices.concat(newChoice);
+    Template.instance().choices.set(currentChoices);
+//    Session.set('newVotingChoices', currentChoices);
+    $('.editing')[0].classList.toggle('hidden');
+    $('.js-add-choice')[0].classList.toggle('hidden');
+  },
+  'click .js-add-choice'(event) {
+    $('.editing')[0].classList.toggle('hidden');
+    $('.js-add-choice')[0].classList.toggle('hidden');
   },
 });
-*/
+
 AutoForm.addModalHooks('af.voting.insert');
 AutoForm.addHooks('af.voting.insert', {
   formToDoc(doc) {
     doc.communityId = Session.get('activeCommunityId');
     doc.userId = Meteor.userId();
     doc.category = 'vote';
+//    doc.vote.choices = afVotingInsertInstance.choices.get();
     return doc;
   },
 });
