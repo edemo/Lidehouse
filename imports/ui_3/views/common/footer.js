f0448682540a61c1c9001ea8620b06467b50dff0
droka <daniel.roka@tutanota.com>
2018-04-20 21:18:53 +0200

    Fixed footer doesnt work well, covers bottom of screen

--- imports/ui_3/views/common/footer.js
+++ imports/ui_3/views/common/footer.js
@@ -7,7 +7,7 @@ import './footer.html';
 Template.Footer.onRendered(function () {
     // FIXED FOOTER
     // Uncomment this if you want to have fixed footer or add 'fixed' class to footer element in html code
-    $('.footer').addClass('fixed');
+    // $('.footer').addClass('fixed');
 });
 
 AutoForm.addHooks('af.feedback', {
