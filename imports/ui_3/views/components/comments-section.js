fb76b7d6d4085689703e9ea048d633046a25b7ec
droka <daniel.roka@tutanota.com>
2018-04-20 19:28:59 +0200

    Board page beta (still with some bugs)

---
+++ imports/ui_3/views/components/comments-section.js
@@ -0,0 +1,76 @@
+/* globals document */
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { moment } from 'meteor/momentjs:moment';
+import { TimeSync } from 'meteor/mizzao:timesync';
+
+import { Comments } from '/imports/api/comments/comments.js';
+import { Topics } from '/imports/api/topics/topics.js';
+
+import './comments-section.html';
+
+Template.Comments_section.onCreated(function commentsSectionOnCreated() {
+  this.autorun(() => {
+    this.subscribe('comments.onTopic', { topicId: this.data._id });
+  });
+});
+
+Template.Comments_section.helpers({
+  topicId() {
+    return this._id;
+  },
+  isVote() {
+    const topic = this;
+    return topic.category === 'vote';
+  },
+  likesCount() {
+    const topic = this;
+    return topic.likesCount();
+  },
+  userLikesThis() {
+    const topic = this;
+    return topic.isLikedBy(Meteor.userId());
+  },
+  commentCount() {
+    return this.commentCounter;
+  },
+  comments() {
+    return Comments.find({ topicId: this._id });
+  },
+  selfAvatar() {
+    return Meteor.user().avatar;
+  },
+});
+
+Template.Comment.helpers({
+  avatar() {
+    return this.user().avatar;
+  },
+  displayUser() {
+    return this.user().fullName();
+  },
+  displayTimeSince() {
+    // momentjs is not reactive, but TymeSync call makes this reactive
+    const serverTimeNow = new Date(TimeSync.serverTime());
+    return moment(this.createdAt).from(serverTimeNow);
+  },
+});
+
+Template.Comments_section.events({
+  'click .js-send-comment'(event) {
+    Meteor.call('comments.insert', {
+      topicId: this._id,
+      userId: Meteor.userId(),
+      text: document.getElementById('text_' + this._id).value,
+    });
+    document.getElementById('text_' + this.topicId).value = '';
+  },
+  'click .js-like'(event) {
+    Meteor.call('like', {
+      coll: 'topics',
+      id: this._id,
+      userId: Meteor.userId(),
+    });
+  },
+});
