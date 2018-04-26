92985aa6fcb921e716f5714eafdaeb1bb68dd00c
droka <daniel.roka@tutanota.com>
2018-04-26 18:28:37 +0200

    Balance shows in red for negative

--- imports/ui_3/views/pages/board.js
+++ imports/ui_3/views/pages/board.js
@@ -38,6 +38,30 @@ Template.Board.helpers({
   },
 });
 
+Template.Balance.helpers({
+  balance() {
+    const userBalance = -14500; //TODO
+    const signPrefix = userBalance > 0 ? '+' : '';
+    return signPrefix + userBalance;
+  },
+  balanceMessage() {
+    const userBalance = -14500; //TODO
+    if (userBalance > 0) return 'Önnek túlfizetése van';
+    else if (userBalance < 0) return 'Önnek tartozása van';
+    return 'Túlfizetés/tartozás';
+  },
+  balanceColorClass() {
+    const userBalance = -14500; //TODO
+    if (userBalance < 0) return 'bg-danger';
+    return 'navy-bg';
+  },
+  balanceIcon() {
+    const userBalance = -14500; //TODO
+    if (userBalance < 0) return 'glyphicon glyphicon-exclamation-sign';
+    return 'fa fa-thumbs-up';
+  },
+});
+
 Template.News.helpers({
   topics(category, stickyVal) {
     const communityId = Session.get('activeCommunityId');
