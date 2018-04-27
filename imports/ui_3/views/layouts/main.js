c1e0e9c48534aa326c41fa8fa42c0a1a3bf6196f
droka <daniel.roka@tutanota.com>
2018-04-27 11:40:56 +0200

    Revert "Simplifying wrapper height recalculation on resize"
    This reverts commit 7217de4ab69f735833f9d5f974205548e76583d3.

--- imports/ui_3/views/layouts/main.js
+++ imports/ui_3/views/layouts/main.js
@@ -59,11 +59,38 @@ Template.Main_layout.onCreated(function() {
 });
 
 Template.Main_layout.onRendered(function() {
-     // Fix height of layout when resize, scroll and load
+    // Minimalize menu when screen is less than 768px
+    $(window).bind("resize load", function () {
+        if ($(this).width() < 769) {
+            $('body').addClass('body-small')
+        } else {
+            $('body').removeClass('body-small')
+        }
+    });
+
+    // Fix height of layout when resize, scroll and load
     $(window).bind("load resize scroll", function() {
-        const windowHeight = $(window).height();
-        const topbarHeight = $('nav.navbar-fixed-top').height();
-        $('#page-wrapper').css("min-height", (windowHeight - topbarHeight) + "px");
+        if(!$("body").hasClass('body-small')) {
+
+            var navbarHeight = $('nav.navbar-default').height();
+            var wrapperHeight = $('#page-wrapper').height();
+
+            if(navbarHeight > wrapperHeight){
+                $('#page-wrapper').css("min-height", navbarHeight + "px");
+            }
+
+            if(navbarHeight < wrapperHeight){
+                $('#page-wrapper').css("min-height", $(window).height()  + "px");
+            }
+
+            if ($('body').hasClass('fixed-nav')) {
+                if (navbarHeight > wrapperHeight) {
+                    $('#page-wrapper').css("min-height", navbarHeight  + "px");
+                } else {
+                    $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
+                }
+            }
+        }
     });
 
 
