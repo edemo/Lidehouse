621cf3b3f0cd443b14643dfdc76704b188b50cfd
droka <daniel.roka@tutanota.com>
2018-06-23 01:12:28 +0200

    Browser policy merging

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,7 +1,6 @@
 import { BrowserPolicy } from 'meteor/browser-policy';
 
 // https://stackoverflow.com/questions/30280370/how-does-content-security-policy-work#30280371
-// Setting taken from https://dweldon.silvrback.com/browser-policy
 
 BrowserPolicy.framing.disallow();
 BrowserPolicy.content.disallowInlineScripts();
@@ -9,4 +8,7 @@ BrowserPolicy.content.disallowEval();
 BrowserPolicy.content.allowInlineStyles();
 BrowserPolicy.content.allowFontDataUrl();
 BrowserPolicy.content.allowSameOriginForAll();
+BrowserPolicy.content.allowOriginForAll('data:');
+BrowserPolicy.content.allowOriginForAll('https://fonts.googleapis.com');
+BrowserPolicy.content.allowOriginForAll('https://fonts.gstatic.com');
 
