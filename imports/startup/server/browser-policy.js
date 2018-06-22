c46739e85a4376f578936e75b8cf6707ec8173da
droka <daniel.roka@tutanota.com>
2018-06-22 19:40:23 +0200

    Update browser policy

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -1,10 +1,12 @@
 import { BrowserPolicy } from 'meteor/browser-policy';
 
 // https://stackoverflow.com/questions/30280370/how-does-content-security-policy-work#30280371
+// Setting taken from https://dweldon.silvrback.com/browser-policy
 
+BrowserPolicy.framing.disallow();
 BrowserPolicy.content.disallowInlineScripts();
-
+BrowserPolicy.content.disallowEval();
+BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowFontDataUrl();
 BrowserPolicy.content.allowSameOriginForAll();
-BrowserPolicy.content.allowOriginForAll('data:');
-BrowserPolicy.content.allowOriginForAll('https://fonts.googleapis.com');
-BrowserPolicy.content.allowOriginForAll('https://fonts.gstatic.com');
+
