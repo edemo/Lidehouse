dd431e23f4bfd0a2b2b469a43ef4ebe53a583d06
kmildi <keleti.ildi@gmail.com>
2018-07-03 17:08:43 +0200

    Voting autoform modification for only poll vote

--- imports/utils/autoform.js
+++ imports/utils/autoform.js
@@ -5,7 +5,8 @@ import { __ } from '/imports/localization/i18n.js';
 export const autoformOptions = function autoformOptions(values, i18Path = '') {
   return {
     options() {
-      return values.map(function option(t) { return { label: __(i18Path + t), value: t }; });
+      const valuesArray = (values instanceof Function) ? values() : values;
+      return valuesArray.map(function option(t) { return { label: __(i18Path + t), value: t }; });
     },
   };
 };
