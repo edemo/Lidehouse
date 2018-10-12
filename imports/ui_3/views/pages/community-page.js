f8b4edfd8724faf59367710d47685a4f80055e5e
droka <daniel.roka@tutanota.com>
2018-10-12 12:16:15 +0200

    Not needed import

--- imports/ui_3/views/pages/community-page.js
+++ imports/ui_3/views/pages/community-page.js
@@ -6,7 +6,7 @@ import { _ } from 'meteor/underscore';
 
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { setRouteBeforeSignin } from '/imports/startup/both/useraccounts-configuration.js';
+
 import { TAPi18n } from 'meteor/tap:i18n';
 import { datatables_i18n } from 'meteor/ephemer:reactive-datatables';
 
