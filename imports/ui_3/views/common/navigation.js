2b180fbeee7772c95bcd058e4d47db246178d5a9
droka <daniel.roka@tutanota.com>
2018-11-01 15:06:02 +0100

    Replace jquery metis with vanilla metis (updated to the latest version)

--- imports/ui_3/views/common/navigation.js
+++ imports/ui_3/views/common/navigation.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
-import { $ } from 'meteor/jquery';
+import MetisMenu from 'metismenujs';
 
 import { Parcels } from '/imports/api/parcels/parcels.js';
 import { Memberships } from '/imports/api/memberships/memberships.js';
@@ -12,7 +12,7 @@ import './navigation.html';
 
 Template.Navigation.onRendered(function() {
   // Initialize metisMenu
-  $('#side-menu').metisMenu({
+  new MetisMenu('#side-menu', {
     toggle: true,
     activeClass: 'active',
     collapseClass: 'collapse',
