4f596ffa69decba2dee22464e1ddc3d46ae4166d
droka <daniel.roka@tutanota.com>
2018-11-02 11:47:21 +0100

    Removing metismenu usage

--- imports/ui_3/views/common/navigation.js
+++ imports/ui_3/views/common/navigation.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
-import MetisMenu from 'metismenujs';
 
 import { Parcels } from '/imports/api/parcels/parcels.js';
 import { Memberships } from '/imports/api/memberships/memberships.js';
@@ -12,14 +11,14 @@ import './navigation.html';
 
 Template.Navigation.onRendered(function() {
   // Initialize metisMenu
-  new MetisMenu('#side-menu', {
+  /* new MetisMenu('#side-menu', {
     toggle: true,
     activeClass: 'active',
     collapseClass: 'collapse',
     collapseInClass: 'in',
     collapsingClass: 'collapsing',
     preventDefault: true,
-  });
+  });*/
 });
 
 Template.Navigation.helpers({
