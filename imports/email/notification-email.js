b47ecdccf1868d392443521a79dc9a6c17769444
droka <daniel.roka@tutanota.com>
2018-12-08 02:11:15 +0100

    Moving category image files to the public folder

--- imports/email/notification-email.js
+++ imports/email/notification-email.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { FlowRouterHelpers } from 'meteor/arillo:flow-router-helpers';
 import { Communities } from '/imports/api/communities/communities.js';
 import { Topics } from '/imports/api/topics/topics.js';
 import { Comments } from '/imports/api/comments/comments.js';
@@ -30,6 +31,17 @@ export const Notification_Email = {
       const user = Meteor.users.findOne(this.userId);
       user.hasNowSeen(topic, Meteor.users.SEEN_BY_NOTI);
       return comments;
+    },
+    categoryImgUrlFor(category) {
+      const file = {
+        // feedback: '',
+        forum: 'font-awesome_4-7-0_commenting_100_0_2d4050_none.png',
+        ticket: 'font-awesome_4-7-0_wrench_100_0_2d4050_none.png',
+        room: 'font-awesome_4-7-0_envelope_100_0_2d4050_none.png',
+        vote: 'font-awesome_4-7-0_gavel_100_0_2d4050_none.png',
+        news: 'font-awesome_4-7-0_exclamation-circle_100_0_2d4050_none.png',
+      };
+      return FlowRouterHelpers.urlFor('/images/email/' + file[category]);
     },
     text() {
       let thingsHappened = false;
