a9d243d3d855d708a05eeb318ed49abc9582246c
droka <daniel.roka@tutanota.com>
2018-12-27 11:21:54 +0100

    Person contact information

--- imports/api/users/person.js
+++ imports/api/users/person.js
@@ -7,19 +7,23 @@ import { autoformOptions, chooseUser } from '/imports/utils/autoform.js';
 import { __ } from '/imports/localization/i18n.js';
 import { Memberships } from '/imports/api/memberships/memberships.js';
 
+const ContactSchema = new SimpleSchema({
+  address: { type: String, optional: true },
+  phone: { type: String, optional: true },
+});
+
 const idCardTypeValues = ['natural', 'legal'];
 const IdCardSchema = new SimpleSchema({
   type: { type: String, allowedValues: idCardTypeValues, autoform: autoformOptions(idCardTypeValues, 'schemaMemberships.person.') },
   name: { type: String, optional: true },
   address: { type: String, optional: true },
-  phone: { type: String, optional: true },
-  identifier: { type: String, optional: true }, // cegjegyzek szam vagy szig szam - egyedi!!!
+  identifier: { type: String, optional: true }, // cegjegyzek szam vagy szig szam - unique!!!
+  dob: { type: Date, optional: true },  // date of birth/company formation
   mothersName: { type: String, optional: true },
-  dob: { type: Date, optional: true },  // date of birth
 });
 
 export const PersonSchema = new SimpleSchema({
-  // The membership is connecting to a person via 3 possible ways: 
+  // The membership is connecting to a person via 3 possible ways:
   // *userId* (connecting to a registered user in the system),
   userId: { type: String, regEx: SimpleSchema.RegEx.Id, optional: true, autoform: chooseUser },
   // *userEmail* (not yet registered, but invitation is sent)
@@ -27,6 +31,7 @@ export const PersonSchema = new SimpleSchema({
   // *idCard* (identity papers confirmed by manager, so person can officially vote now)
   // this person might or might not wish to register in the system ever, but still can do voting (if manager votes in his name)
   idCard: { type: IdCardSchema, optional: true },
+  contact: { type: ContactSchema, optional: true },
 });
 
 PersonSchema.modifiableFields = [
@@ -46,6 +51,7 @@ export class Person {
     this.userId = data.userId;
     this.userEmail = data.userEmail;
     this.idCard = data.idCard;
+    this.contact = data.contact;
   }
   // A personId is either a userId (for registered users) or an idCard identifier (for non-registered users)
   static constructFromId(personId) {
@@ -55,16 +61,22 @@ export class Person {
     if (user) {
       data.userId = personId;
       const m1 = Memberships.findOne({ 'person.userId': personId });
-      if (m1) data.idCard = m1.person.idCard;
+      if (m1) {
+        data.idCard = m1.person.idCard;
+        data.contact = m1.person.contact;
+      }
     } else {
       const m2 = Memberships.findOne({ 'person.idCard.identifier': personId });
-      if (m2) data.idCard = m2.person.idCard;
+      if (m2) {
+        data.idCard = m2.person.idCard;
+        data.contact = m2.person.contact;
+      }
     }
     return new Person(data);
   }
   isConsistent() {
     if (this.userId && this.userEmail) return false;
-    return true; 
+    return true;
   }
   isVerified() {
     return !!this.idCard;
