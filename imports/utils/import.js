b55b37a224c88e7c70dfb6e6d2d415e8b3b86ded
droka <daniel.roka@tutanota.com>
2018-12-28 22:32:39 +0100

    Really fix previous commit

--- imports/utils/import.js
+++ imports/utils/import.js
@@ -45,7 +45,7 @@ function transformMarinaMemberships(jsons) {
     doc.person.idCard.type = 'natural';
     doc.role = 'owner';
     const names = doc.owners.split(/,|;|\n/);
-    const emails = doc.emails.split(/,|;|\n/);
+    const emails = doc.emails ? doc.emails.split(/,|;|\n/) : [];
     names.forEach((name) => {
       const tdoc = {}; $.extend(true, tdoc, doc);
       tdoc.person.idCard.name = name;
