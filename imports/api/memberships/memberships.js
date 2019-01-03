ef69fb12a3758b33f180e6fd5eb1fb396a150622
droka <daniel.roka@tutanota.com>
2019-01-03 14:08:05 +0100

    Fixing unit tests (missing fractional import)

--- imports/api/memberships/memberships.js
+++ imports/api/memberships/memberships.js
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Fraction } from 'fractional';
+import '/imports/startup/both/fractional.js';  // TODO: should be automatic, but not included in tests
 
 import { __ } from '/imports/localization/i18n.js';
 import { debugAssert } from '/imports/utils/assert.js';
