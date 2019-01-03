777df5e1d2360f6ad5fc748ff36fdf9c148a2f28
droka <daniel.roka@tutanota.com>
2019-01-03 13:13:20 +0100

    Tests expectations for revision edge cases

--- imports/api/revision.tests.js
+++ imports/api/revision.tests.js
@@ -11,7 +11,6 @@ if (Meteor.isServer) {
   describe('revision', function () {
     this.timeout(5000);
 
-    console.log('in describe');
     const schema = new SimpleSchema({
       textField: { type: String, optional: true },
       nonRevisionedField: { type: String, optional: true },
@@ -31,50 +30,53 @@ if (Meteor.isServer) {
         });
       });
 
-      it('insertion', function (done) {
+      it('inserts docs', function (done) {
         const doc = collection.findOne({ textField: 'text' });
-        chai.assert.isDefined(doc);
         chai.assert.equal(doc.nonRevisionedField, 'This is not revisioned');
         chai.assert.equal(doc.booleanField, false);
+        chai.assert.isUndefined(doc.revision);
         done();
       });
 
-      it('update string field', function (done) {
+      it('update revisions string field', function (done) {
         collection.update({ textField: 'text' }, { $set: { textField: 'newText' } });
         const doc = collection.findOne({ textField: 'text' });
         chai.assert.isUndefined(doc);
         const updatedDoc = collection.findOne({ textField: 'newText' });
-        chai.assert.isDefined(updatedDoc);
-        chai.assert.isDefined(updatedDoc.revision);
         chai.assert.equal(updatedDoc.revision.length, 1);
         chai.assert.equal(updatedDoc.revision[0].field, 'textField');
         chai.assert.equal(updatedDoc.revision[0].oldValue, 'text');
         done();
       });
 
-      it('update non-revisioned field', function (done) {
+      it('update doesnt revision for non-revisioned field', function (done) {
         collection.update(docId, { $set: { nonRevisionedField: 'Still not revisioned' } });
         const updatedDoc = collection.findOne(docId);
-        chai.assert.isDefined(updatedDoc);
         chai.assert.equal(updatedDoc.nonRevisionedField, 'Still not revisioned');
-        chai.assert.isDefined(updatedDoc.revision);
         chai.assert.equal(updatedDoc.revision.length, 1);
         done();
       });
 
-      it('update boolean field', function (done) {
+      it('update doesnt revision if same value comes', function (done) {
+        collection.update(docId, { $set: { textField: 'newText' } });
+        const updatedDoc = collection.findOne(docId);
+        chai.assert.equal(updatedDoc.revision.length, 1);
+        chai.assert.equal(updatedDoc.revision[0].field, 'textField');
+        chai.assert.equal(updatedDoc.revision[0].oldValue, 'text');
+        done();
+      });
+
+      it('update revisions boolean field', function (done) {
         collection.update(docId, { $set: { booleanField: true } });
         const updatedDoc = collection.findOne(docId);
-        chai.assert.isDefined(updatedDoc);
         chai.assert.equal(updatedDoc.booleanField, true);
-        chai.assert.isDefined(updatedDoc.revision);
         chai.assert.equal(updatedDoc.revision.length, 2);
         chai.assert.equal(updatedDoc.revision[0].field, 'booleanField');
         chai.assert.equal(updatedDoc.revision[0].oldValue, false);
         done();
       });
 
-      it('remove', function (done) {
+      it('removes docs', function (done) {
         collection.remove(docId);
         const doc = collection.findOne(docId);
         chai.assert.isUndefined(doc);
@@ -91,43 +93,39 @@ if (Meteor.isServer) {
         });
       });
 
-      it('update to empty string', function (done) {
-        collection.update(docId, { $set: { textField: '' } });
+      afterEach(function () {
+        collection.remove(docId);
+      });
+
+      it('handles unset field', function (done) {
+        collection.update(docId, { $unset: { textField: '' } });
         let updatedDoc = collection.findOne(docId);
-        chai.assert.isDefined(updatedDoc);
-        chai.assert.equal(updatedDoc.textField, '');
-        chai.assert.isDefined(updatedDoc.revision);
+        chai.assert.isUndefined(updatedDoc.textField);
         chai.assert.equal(updatedDoc.revision.length, 1);
         chai.assert.equal(updatedDoc.revision[0].field, 'textField');
         chai.assert.equal(updatedDoc.revision[0].oldValue, 'text');
-      
+
         collection.update(docId, { $set: { textField: 'newText' } });
         updatedDoc = collection.findOne(docId);
         chai.assert.equal(updatedDoc.revision.length, 2);
         chai.assert.equal(updatedDoc.revision[0].field, 'textField');
-        chai.assert.equal(updatedDoc.revision[0].oldValue, '');
+        chai.assert.isUndefined(updatedDoc.revision[0].oldValue);
         done();
       });
 
-      it('delete field with unset', function (done) {
-        collection.update(docId, { $unset: { textField: 0 } });
-        const updatedDoc = collection.findOne(docId);
-        chai.assert.isDefined(updatedDoc);
+      it('handles empty string', function (done) {
+        collection.update(docId, { $set: { textField: '' } });
+        let updatedDoc = collection.findOne(docId);
         chai.assert.isUndefined(updatedDoc.textField);
         chai.assert.equal(updatedDoc.revision.length, 1);
         chai.assert.equal(updatedDoc.revision[0].field, 'textField');
         chai.assert.equal(updatedDoc.revision[0].oldValue, 'text');
-        done();
-      });
-
-      it('delete field with set to undefined', function (done) {
-        collection.update(docId, { $set: { textField: undefined } });
-        const updatedDoc = collection.findOne(docId);
-        chai.assert.isDefined(updatedDoc);
-        chai.assert.isUndefined(updatedDoc.textField);
-        chai.assert.equal(updatedDoc.revision.length, 1);
+ 
+        collection.update(docId, { $set: { textField: 'newText' } });
+        updatedDoc = collection.findOne(docId);
+        chai.assert.equal(updatedDoc.revision.length, 2);
         chai.assert.equal(updatedDoc.revision[0].field, 'textField');
-        chai.assert.equal(updatedDoc.revision[0].oldValue, 'text');
+        chai.assert.isUndefined(updatedDoc.revision[0].oldValue);
         done();
       });
     });
