b6822d18a01b0da803664ef0cb22ab9afee5fcdd
TMSBK <frguitar@gmail.com>
2019-01-29 15:57:31 +0100

    Minor fixes

--- imports/ui_3/views/components/envelope.js
+++ imports/ui_3/views/components/envelope.js
@@ -5,23 +5,21 @@ import { $ } from 'meteor/jquery';
 export function envelope(templateName, ReactiveDictName, ReactiveDictData) {
 
   Template[templateName].onRendered(function () {
-    this.$('.letter-content').wrap($('<div>', { class: 'letter' }));
-    this.$('.letter').wrap($('<div>', { class: 'envelope-letter' }));
+    this.$('.letter-content').wrap($('<div>', { class: 'envelope-letter' }));
     this.$('.envelope-letter').wrap($('<div>', { class: 'envelope' }));
     this.$('.envelope').prepend($('<div>', { class: 'envelope-flap' }));
-    this.$('.envelope').prepend($('<input>', { class: 'envelope-check' }));
     this.$('.envelope').append($('<div>', { class: 'envelope-back' }));
 
     this.autorun(() => {
       const value = Template.instance()[ReactiveDictName].get(ReactiveDictData);
       if (value) {
-        this.$('.letter').removeClass('voted-letter');
+        this.$('.envelope-letter').removeClass('voted-letter');
         this.$('.envelope-letter').removeClass('letter-animation-reverse');
         this.$('.envelope-flap').removeClass('flap-animation-reverse');
         this.$('.envelope-letter').addClass('letter-animation');
         this.$('.envelope-flap').addClass('flap-animation');
       } else {
-        this.$('.letter').addClass('voted-letter');
+        this.$('.envelope-letter').addClass('voted-letter');
         this.$('.envelope-letter').removeClass('letter-animation');
         this.$('.envelope-flap').removeClass('flap-animation');
         this.$('.envelope-letter').addClass('letter-animation-reverse');
