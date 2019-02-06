0c6ae8bebdb31916e9e10ee20acff53c7e2d8c8b
TMSBK <frguitar@gmail.com>
2019-02-06 11:33:45 +0100

    Refactoring vote cast panel (separate it from votebox & unifying types)

---
+++ imports/ui_3/views/components/vote-cast-panel.js
@@ -0,0 +1,189 @@
+/* globals document */
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { $ } from 'meteor/jquery';
+import { _ } from 'meteor/underscore';
+
+import { __ } from '/imports/localization/i18n.js';
+import { onSuccess, displayMessage } from '/imports/ui_3/lib/errors.js';
+import { Topics } from '/imports/api/topics/topics.js';
+import { castVote } from '/imports/api/topics/votings/methods.js';
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
+import '../components/select-voters.js';
+import './vote-cast-panel.html';
+import { createEnvelope } from './envelope.js';
+
+Template.Vote_cast_panel.onCreated(function voteCastPanelOnCreated() {
+  this.state = new ReactiveDict();
+  this.state.set('choice', undefined);
+});
+
+Template.Vote_cast_panel.onRendered(function voteboxOnRendered() {
+  const self = this;
+  const state = this.state;
+  const topicId = this.data._id;
+  const vote = this.data.vote;
+  const voteCasts = this.data.voteCasts;
+
+  const voteEnvelope = createEnvelope(this.$('.letter-content'));
+  this.autorun(() => {
+    if (state.get('voteIsFinalized')) voteEnvelope.close();
+    else voteEnvelope.open();
+  });
+  // creating the JQuery sortable widget
+  // both JQuery and Blaze wants control over the order of elements, so needs a hacky solution
+  // https://github.com/meteor/meteor/blob/master/examples/unfinished/reorderable-list/client/shark.js
+  // https://differential.com/insights/sortable-lists-in-meteor-using-jquery-ui/
+  $(self.find('.sortable')).sortable({
+    stop(event, ui) { // fired when an item is dropped
+      event.preventDefault();
+      const preference = $(self.find('.sortable')).sortable('toArray', { attribute: 'data-value' })
+        .map(function obj(value, index) { return { text: vote.choices[value], value }; });
+//      console.log('onstop:', preference);
+      state.set('preference', preference);
+    },
+  });
+
+  // this is in an autorun, so if logged in user changes, it will rerun
+  // or if the vote is changed on another machine, it also gets updated here
+  this.autorun(function update() {
+
+    const voting = Topics.findOne(topicId);
+    const voteIsFinalized = voting.hasVoted(Meteor.userId());
+    state.set('voteIsFinalized', voteIsFinalized);
+    let preference;
+    if (voteIsFinalized) {
+      const castedPreference = voting.voteOf(Meteor.userId());
+      preference = castedPreference.map(function obj(value) { return { text: vote.choices[value], value }; });
+    } else { // no vote yet, preference is then the original vote choices in that order
+      preference = vote.choices.map(function obj(text, index) { return { text, value: index }; });
+    }
+    state.set('preference', preference);
+//  console.log('onrender:', preference);
+  });
+
+  // This is where we enable/disable the sorting, dependant on the finalized state
+  this.autorun(function update() {
+    const voteIsFinalized = state.get('voteIsFinalized');
+    $(self.find('.sortable')).sortable(voteIsFinalized ? 'disable' : 'enable');
+   /* const voting = Topics.findOne(topicId);
+    const hasVoted = voting.hasVoted(Meteor.userId());
+    $(Template.instance().find('.sortable')).sortable(hasVoted ? 'disable' : 'enable');
+  */
+  });
+});
+
+Template.Vote_cast_panel.helpers({
+  indirectUser() {
+    // TODO: Return the user, who voted in my name
+    return Object.keys(this.voteCasts)[Object.keys(this.voteCasts).length - 1];
+  },
+  avatarForEnvelope(userId) {
+    return Meteor.users.findOne({ _id: userId }).avatar;
+  },
+  isButtonLayoutVertical() {
+    return this.vote.type === 'preferential';
+  },
+  // Single choice voting
+  pressedClassForVoteBtn(choice) {
+    const userId = Meteor.userId();
+    const votedChoice = this.voteOf(userId);
+    const voteIsFinalized = Template.instance().state.get('voteIsFinalized');
+    const temporaryChoice = Template.instance().state.get('choice');
+    if (voteIsFinalized) return _.isEqual(votedChoice, [choice]) && 'btn-pressed';
+    return choice === temporaryChoice && 'btn-pressed';
+  },
+  // Preferential voting
+  currentPreference() {
+    const preference = Template.instance().state.get('preference');
+//  console.log('ondisplay:', preference);
+    return preference;
+  },
+  voteOfUser() {
+    return this.voteOf(Meteor.userId());
+  },
+  voteIsFinalized() {
+    return Template.instance().state.get('voteIsFinalized');
+    //const hasVoted = this.hasVoted(Meteor.userId());
+   // return hasVoted;
+  },
+  stateClassForSendButton() {
+    const voteIsFinalized = Template.instance().state.get('voteIsFinalized');
+    const temporaryChoice = Template.instance().state.get('choice');
+    if (_.isDefined(temporaryChoice) && !voteIsFinalized) return 'visible-button';
+    return 'invisible-button';
+  },
+});
+
+function castVoteBasedOnPermission(topicId, castedVote, callback) {
+  const communityId = Session.get('activeCommunityId');
+  if (Meteor.user().hasPermission('vote.castForOthers', communityId)) {
+    const modalContext = {
+      title: 'Proxy voting',
+      body: 'Select_voters',
+      bodyContext: _.extend(this, { topicId, castedVote }),
+      btnClose: 'cancel',
+      btnOK: 'Send vote',
+      onOK() {
+        castVote.call(
+          { topicId, castedVote, voters: AutoForm.getFieldValue('voters', 'af.select.voters') },
+          callback
+        );
+      },
+    };
+    Modal.show('Modal', modalContext);
+  } else {
+    castVote.call({ topicId, castedVote }, callback);
+  }
+}
+
+Template.Vote_cast_panel.events({
+  // event handler for the single choice vote type
+  'click .btn-vote'(event) {
+    const voteIsFinalized = Template.instance().state.get('voteIsFinalized');
+    if (!voteIsFinalized) {
+      Template.instance().state.set('choice', $(event.target).closest('.btn').data('value'));
+    } else {
+      return;
+    }
+  },
+  'click .send-button'(event, instance) {
+    const topicId = this._id;
+    if (this.vote.type === 'preferential') {
+      const voteIsFinalized = instance.state.get('voteIsFinalized');
+      if (!voteIsFinalized) {
+        const preference = instance.state.get('preference');
+        const castedVote = preference.map(p => p.value);
+        castVoteBasedOnPermission(topicId, castedVote,
+          onSuccess((res) => {
+            displayMessage('success', 'Vote casted');
+          })
+        );
+      } else { // voteIsFinalized === true
+        instance.state.set('voteIsFinalized', false);
+      }
+    } else {
+      const temporaryChoice = Template.instance().state.get('choice');
+      castVoteBasedOnPermission(topicId, [temporaryChoice],
+        onSuccess(res => displayMessage('success', 'Vote casted'))
+      );
+    }
+  },
+  'mousedown .btn-vote-choice'() {
+    Template.instance().state.set('choice', 1);
+  },
+});
+
+Template.User_vote_status.events({
+  'click .js-revoke'(event) {
+    const topicId = this._id;
+    const vote = [];  // indicates a no-vote
+    castVote.call({ topicId, castedVote: vote },
+      onSuccess(res => displayMessage('success', 'Vote revoked'))
+    );
+    Template.instance().state.set('choice', undefined);
+  },
+});
--- imports/ui_3/views/components/votebox.js
+++ imports/ui_3/views/components/votebox.js
@@ -3,8 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { AutoForm } from 'meteor/aldeed:autoform';
 import { $ } from 'meteor/jquery';
 import { _ } from 'meteor/underscore';
 
@@ -23,15 +21,13 @@ import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import '/imports/ui_3/views/modals/voting-edit.js';
 import '/imports/ui_3/views/components/vote-results.js';
 import '../components/select-voters.js';
+import './vote-cast-panel.js';
 import './votebox.html';
-import { createEnvelope } from './envelope.js';
 
 const choiceColors = ['#a3e1d4', '#ed5565', '#b5b8cf', '#9CC3DA', '#f8ac59']; // colors taken from the theme
 const notVotedColor = '#dedede';
 
 Template.Votebox.onCreated(function voteboxOnCreated() {
-  this.state = new ReactiveDict();
-  this.state.set('choice', undefined);
   this.autorun(() => {
     const communityId = Session.get('activeCommunityId');
     const topicId = this.data._id;
@@ -40,58 +36,8 @@ Template.Votebox.onCreated(function voteboxOnCreated() {
 });
 
 Template.Votebox.onRendered(function voteboxOnRendered() {
-  const self = this;
-  const state = this.state;
   const topicId = this.data._id;
   const vote = this.data.vote;
-  const voteCasts = this.data.voteCasts;
-
-  const voteEnvelope = createEnvelope(this.$('.letter-content'));
-  this.autorun(() => {
-    if (state.get('voteIsFinalized')) voteEnvelope.close();
-    else voteEnvelope.open();
-  });
-  // creating the JQuery sortable widget
-  // both JQuery and Blaze wants control over the order of elements, so needs a hacky solution
-  // https://github.com/meteor/meteor/blob/master/examples/unfinished/reorderable-list/client/shark.js
-  // https://differential.com/insights/sortable-lists-in-meteor-using-jquery-ui/
-  $(self.find('.sortable')).sortable({
-    stop(event, ui) { // fired when an item is dropped
-      event.preventDefault();
-      const preference = $(self.find('.sortable')).sortable('toArray', { attribute: 'data-value' })
-        .map(function obj(value, index) { return { text: vote.choices[value], value }; });
-//      console.log('onstop:', preference);
-      state.set('preference', preference);
-    },
-  });
-
-  // this is in an autorun, so if logged in user changes, it will rerun
-  // or if the vote is changed on another machine, it also gets updated here
-  this.autorun(function update() {
-
-    const voting = Topics.findOne(topicId);
-    const voteIsFinalized = voting.hasVoted(Meteor.userId());
-    state.set('voteIsFinalized', voteIsFinalized);
-    let preference;
-    if (voteIsFinalized) {
-      const castedPreference = voting.voteOf(Meteor.userId());
-      preference = castedPreference.map(function obj(value) { return { text: vote.choices[value], value }; });
-    } else { // no vote yet, preference is then the original vote choices in that order
-      preference = vote.choices.map(function obj(text, index) { return { text, value: index }; });
-    }
-    state.set('preference', preference);
-//  console.log('onrender:', preference);
-  });
-  
-  // This is where we enable/disable the sorting, dependant on the finalized state
-  this.autorun(function update() {
-    const voteIsFinalized = state.get('voteIsFinalized');
-    $(self.find('.sortable')).sortable(voteIsFinalized ? 'disable' : 'enable');
-   /* const voting = Topics.findOne(topicId);
-    const hasVoted = voting.hasVoted(Meteor.userId());
-    $(Template.instance().find('.sortable')).sortable(hasVoted ? 'disable' : 'enable');
-  */
- });
   // Filling the chart with data
   this.autorun(() => {
     const voting = Topics.findOne(topicId);
@@ -128,74 +74,13 @@ Template.Votebox.helpers({
   avatar() {
     return Meteor.users.findOne(this.userId).avatar;
   },
-  indirectUser() {
-    // TODO: Return the user, who voted in my name
-    return Object.keys(this.voteCasts)[Object.keys(this.voteCasts).length - 1];
-  },
-  avatarForEnvelope(userId) {
-    return Meteor.users.findOne({ _id: userId }).avatar;
-  },
   comments() {
     return Comments.find({ topicId: this._id }, { sort: { createdAt: 1 } });
-  },
-  isButtonLayoutVertical() {
-    return this.vote.type === 'preferential';
-  },
-  // Single choice voting
-  pressedClassForVoteBtn(choice) {
-    const userId = Meteor.userId();
-    const votedChoice = this.voteOf(userId);
-    const voteIsFinalized = Template.instance().state.get('voteIsFinalized');
-    const temporaryChoice = Template.instance().state.get('choice');
-    if (voteIsFinalized) return _.isEqual(votedChoice, [choice]) && 'btn-pressed';
-    return choice === temporaryChoice && 'btn-pressed';
-  },
-  // Preferential voting
-  currentPreference() {
-    const preference = Template.instance().state.get('preference');
-//  console.log('ondisplay:', preference);
-    return preference;
-  },
-  voteOfUser() {
-    return this.voteOf(Meteor.userId());
-  },
-  voteIsFinalized() {
-    return Template.instance().state.get('voteIsFinalized');
-    //const hasVoted = this.hasVoted(Meteor.userId());
-   // return hasVoted;
   },
   attachments() {
     return Shareddocs.find({ topicId: this._id });
   },
-  stateClassForSendButton() {
-    const voteIsFinalized = Template.instance().state.get('voteIsFinalized');
-    const temporaryChoice = Template.instance().state.get('choice');
-    if (_.isDefined(temporaryChoice) && !voteIsFinalized) return 'visible-button';
-    return 'invisible-button';
-  },
 });
-
-function castVoteBasedOnPermission(topicId, castedVote, callback) {
-  const communityId = Session.get('activeCommunityId');
-  if (Meteor.user().hasPermission('vote.castForOthers', communityId)) {
-    const modalContext = {
-      title: 'Proxy voting',
-      body: 'Select_voters',
-      bodyContext: _.extend(this, { topicId, castedVote }),
-      btnClose: 'cancel',
-      btnOK: 'Send vote',
-      onOK() {
-        castVote.call(
-          { topicId, castedVote, voters: AutoForm.getFieldValue('voters', 'af.select.voters') },
-          callback
-        );
-      },
-    };
-    Modal.show('Modal', modalContext);
-  } else {
-    castVote.call({ topicId, castedVote }, callback);
-  }
-}
 
 Template.Votebox.events({
   'click .js-edit'(event) {
@@ -220,59 +105,6 @@ Template.Votebox.events({
       action: 'delete topic',
       message: 'It will disappear forever',
     });
-  },
-  'click .btn-golive'(event) {
-    const modalContext = {
-      title: 'Live voting',
-      body: 'Votebox',
-      bodyContext: _.extend(this, { live: true }),
-      btnClose: 'cancel',
-      btnOK: 'send vote',
-    };
-    Modal.show('Modal', modalContext);
-  },
-  // event handler for the single choice vote type
-  'click .btn-vote'(event) {
-    const voteIsFinalized = Template.instance().state.get('voteIsFinalized');
-    if (!voteIsFinalized) {
-      Template.instance().state.set('choice', $(event.target).closest('.btn').data('value'));
-    } else {
-      return;
-    }
-  },
-  'click .send-button'(event) {
-    const topicId = this._id;
-    const temporaryChoice = Template.instance().state.get('choice');
-    castVoteBasedOnPermission(topicId, [temporaryChoice],
-      onSuccess(res => displayMessage('success', 'Vote casted'))
-    );
-  },
-  'mousedown .btn-vote-choice'() {
-    Template.instance().state.set('choice', 1);
-  },
-  // event handler for the preferential vote type
-  'click .send-button-finalize'(event, instance) {
-    const topicId = this._id;
-    const voteIsFinalized = instance.state.get('voteIsFinalized');
-    if (!voteIsFinalized) {
-      const preference = instance.state.get('preference');
-      const castedVote = preference.map(p => p.value);
-      castVoteBasedOnPermission(topicId, castedVote,
-        onSuccess((res) => {
-          displayMessage('success', 'Vote casted');
-        })
-      );
-    } else { // voteIsFinalized === true
-      instance.state.set('voteIsFinalized', false);
-    }
-  },
-  'click .js-revoke'(event) {
-    const topicId = this._id;
-    const vote = [];  // indicates a no-vote
-    castVote.call({ topicId, castedVote: vote },
-      onSuccess(res => displayMessage('success', 'Vote revoked'))
-    );
-    Template.instance().state.set('choice', undefined);
   },
   'click .js-close'(event, instance) {
     const serverTimeNow = new Date(TimeSync.serverTime());
