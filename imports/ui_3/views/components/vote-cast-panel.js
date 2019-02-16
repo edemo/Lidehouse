f7c4009acfb44d119dc1e2d32c70cbf4fead54ed
droka <daniel.roka@tutanota.com>
2019-02-16 21:13:18 +0100

    Simplifying refactor

--- imports/ui_3/views/components/vote-cast-panel.js
+++ imports/ui_3/views/components/vote-cast-panel.js
@@ -8,6 +8,7 @@ import { $ } from 'meteor/jquery';
 import { _ } from 'meteor/underscore';
 
 import { __ } from '/imports/localization/i18n.js';
+import { debugAssert } from '/imports/utils/assert.js';
 import { onSuccess, displayMessage } from '/imports/ui_3/lib/errors.js';
 import { Topics } from '/imports/api/topics/topics.js';
 import { castVote } from '/imports/api/topics/votings/methods.js';
@@ -42,8 +43,6 @@ function castVoteBasedOnPermission(topicId, castedVote, callback) {
 Template.Vote_cast_panel.viewmodel({
   topic: undefined,
   temporaryVote: undefined,
-  preference: undefined,
-  hasVotedState: false,
   onCreated(instance) {
     instance.firstRun = true;
     this.topic(instance.data);
@@ -60,78 +59,47 @@ Template.Vote_cast_panel.viewmodel({
       instance.$('.sortable').sortable({
         stop(event, ui) { // fired when an item is dropped
           event.preventDefault();
-          const preference = instance.$('.sortable').sortable('toArray', { attribute: 'data-value' })
-            .map(function obj(value, index) { return { text: vote.choices[value], value }; });
-    //      console.log('onstop:', preference);
-          self.temporaryVote(preference);
+          const choiceList = instance.$('.sortable').sortable('toArray', { attribute: 'data-value' });
+          self.temporaryVote(choiceList);
         },
       });
     }
 
     instance.voteEnvelope = createEnvelope(instance.$('.letter-content'));
   },
+  registeredVote() {
+    return this.topic().voteOf(Meteor.userId());
+  },
   autorun: [
     function maintainLiveData() {
-      this.topic(Topics.findOne(this.templateInstance.data._id)); 
-    },
-    function syncHasVotedState() {
-      const hasVotedState = this.topic().hasVoted(Meteor.userId());
-      this.hasVotedState(hasVotedState);
-    },
-    function syncPreference() {
-      let preference;
-      const voting = this.topic();
-      if (this.hasVotedState()) {
-        const castedPreference = voting.voteOf(Meteor.userId());
-        preference = castedPreference.map(function obj(value) { return { text: voting.vote.choices[value], value }; });
-      } else { // no vote yet, preference is then the original vote choices in that order
-        preference = voting.vote.choices.map(function obj(text, index) { return { text, value: index }; });
-      }
-      this.preference(preference);
+      this.topic(Topics.findOne(this.templateInstance.data._id));
     },
     function enableDisableSortableState() {
-      this.templateInstance.$('.sortable')
-        .sortable((this.hasVotedState() && this.temporaryVote() === undefined) ? 'disable' : 'enable');
+      this.templateInstance.$('.sortable').sortable(
+        (this.registeredVote() && !this.temporaryVote()) ? 'disable' : 'enable'
+      );
     },
     function openCloseEnvelope() {
-      if (this.hasVotedState() && this.templateInstance.firstRun) {
-        this.templateInstance.voteEnvelope.closed();
+      if (this.templateInstance.firstRun) {
+        if (this.registeredVote()) this.templateInstance.voteEnvelope.closed();
+        else this.templateInstance.voteEnvelope.opened();
+        this.templateInstance.firstRun = false;
+      } else { // not firstRun
+        if (this.registeredVote() && !this.temporaryVote()) {
+          this.templateInstance.voteEnvelope.close();
+        }
+        if (this.registeredVote() && this.temporaryVote()) {
+          this.templateInstance.voteEnvelope.open();
+        }
       }
-      if (!this.hasVotedState() && this.templateInstance.firstRun) {
-        this.templateInstance.voteEnvelope.opened();
-      }
-      if (this.hasVotedState() && !this.templateInstance.firstRun && _.isUndefined(this.temporaryVote())) {
-        this.templateInstance.voteEnvelope.close();
-      }
-      if (this.hasVotedState() && !this.templateInstance.firstRun && _.isDefined(this.temporaryVote())) {
-        this.templateInstance.voteEnvelope.open();
-      }
-      this.templateInstance.firstRun = false;
     },
   ],
-
   /*indirectUser() {
     const myVotePath = this.votePaths[Meteor.userId()];
     const myFirstDelegateId = myVotePath[1];
     const myFirstDelegate = Meteor.users.findOne(myFirstDelegateId);
     return myFirstDelegate;
   },*/
-  originalState() {
-    if (!this.hasVotedState() && this.temporaryVote() === undefined) return true;
-    return false;
-  },
-  votingState() {
-    if (!this.hasVotedState() && _.isDefined(this.temporaryVote())) return true;
-    return false;
-  },
-  castedVoteState() {
-    if (this.hasVotedState() && this.temporaryVote() === undefined) return true;
-    return false;
-  },
-  modifyState() {
-    if (this.hasVotedState() && _.isDefined(this.temporaryVote())) return true;
-    return false;
-  },
   voterAvatar() {
     const userId = Meteor.userId();
     const user = Meteor.users.findOne(userId);
@@ -146,78 +114,54 @@ Template.Vote_cast_panel.viewmodel({
     }
     return undefined;
   },
-  isButtonLayoutVertical() {
-    return this.topic().vote.type === 'preferential';
-  },
   // Single choice voting
   pressedClassForVoteBtn(choice) {
-    const userId = Meteor.userId();
-    const votedChoice = this.topic().voteOf(userId);
-    if (this.votingState() || this.modifyState()) return choice === this.temporaryVote() && 'btn-pressed';
-    if (this.castedVoteState()) return _.isEqual(votedChoice, [choice]) && 'btn-pressed';
+    if (this.temporaryVote()) return _.isEqual([choice], this.temporaryVote()) && 'btn-pressed';
+    if (this.registeredVote()) return _.isEqual([choice], this.registeredVote()) && 'btn-pressed';
     return undefined;
   },
   // Preferential voting
-  currentPreference() {
-    if (this.votingState() || this.modifyState()) {
-      return this.temporaryVote();
+  preference() {
+    const choices = this.topic().vote.choices;
+    if (this.temporaryVote()) {
+      return this.temporaryVote().map(function obj(value) { return { text: choices[value], value }; });
+    } else if (this.registeredVote()) {
+      return this.registeredVote().map(function obj(value) { return { text: choices[value], value }; });
     }
-    if (this.castedVoteState() || this.originalState()) {
-      return this.preference();
-    }
-    return undefined;
-//  console.log('ondisplay:', preference);
-  },
-  voteOfUser() {
-    return this.topic().voteOf(Meteor.userId());
-  },
-  stateClassForSendButton() {
-    if (this.votingState() || this.modifyState()) return 'visible-button';
-    if (this.originalState() || this.castedVoteState()) return 'invisible-button';
-    return undefined;
+    // no vote yet, preference is then the original vote choices in that order
+    return choices.map(function obj(text, index) { return { text, value: index }; });
   },
   textForVote() {
-    if (this.castedVoteState()) return 'Modify vote';
-    return 'Cancel';
+    // originalState
+    if (!this.registeredVote() && !this.temporaryVote()) return 'originalState';
+    // votingState
+    if (!this.registeredVote() && this.temporaryVote()) return 'votingState';
+    // castedVoteState
+    if (this.registeredVote() && !this.temporaryVote()) return 'Modify vote';
+    // modifyState
+    if (this.registeredVote() && this.temporaryVote()) return 'Cancel';
   },
   events: {
-    // event handler for the single choice vote type
-    'click .btn-vote'(event, instance) {
-      if (this.votingState() || this.modifyState() || this.originalState()) {
-        this.temporaryVote($(event.target).closest('.btn').data('value'));
-      } else {
-        return;
-      }
+    'click .btn-vote'(event, instance) {  // event handler for the single choice vote type
+      if (this.registeredVote() && !this.temporaryVote()) return;
+      const selecetedChoice = $(event.target).closest('.btn').data('value');
+      this.temporaryVote([selecetedChoice]);
     },
-    'click .send-button'(event, instance) {
+    'click .btn-send-vote'(event, instance) {
       const topicId = this.topic()._id;
-      if (this.topic().vote.type === 'preferential') {
-        if (this.votingState() || this.modifyState()) {
-          const preference = this.temporaryVote();
-          const castedVote = preference.map(p => p.value);
-          castVoteBasedOnPermission(topicId, castedVote,
-            onSuccess((res) => {
-              displayMessage('success', 'Vote casted');
-            })
-          );
-        }
-      } else {
-        castVoteBasedOnPermission(topicId, [this.temporaryVote()],
-          onSuccess(res => displayMessage('success', 'Vote casted'))
-        );
-      }
+      debugAssert(this.temporaryVote());
+      castVoteBasedOnPermission(topicId, this.temporaryVote(),
+        onSuccess((res) => {
+          displayMessage('success', 'Vote casted');
+        })
+      );
       this.temporaryVote(undefined);
     },
     'click .js-modify'(event, instance) {
-      const userId = Meteor.userId();
       if (this.temporaryVote()) {
         this.temporaryVote(undefined);
-      } else if (this.hasVotedState()) {
-        if (this.topic().vote.type === 'preferential') {
-          this.temporaryVote(this.preference());
-        } else {
-          this.temporaryVote(this.topic().voteOf(userId)[0]);
-        }
+      } else if (this.registeredVote()) {
+        this.temporaryVote(this.registeredVote());
       }
     },
   },
