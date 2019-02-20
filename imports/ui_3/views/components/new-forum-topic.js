c1e98f54c60bfab2cb0175e638d73d406ab4aac7
kmildi <keleti.ildi@gmail.com>
2019-02-20 11:08:01 +0100

    Disable send button at new forum topic if text field is empty

--- imports/ui_3/views/components/new-forum-topic.js
+++ imports/ui_3/views/components/new-forum-topic.js
@@ -1,13 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
+import { $ } from 'meteor/jquery';
 import { insert as insertTopic } from '/imports/api/topics/methods.js';
 import { onSuccess } from '/imports/ui_3/lib/errors.js';
 
 import './new-forum-topic.html';
 
 Template.New_forum_topic.events({
+  'input #new_forum_text'(event) {
+    if (event.target.value === '') {
+      $(event.target).closest('.media').find('.js-send').addClass('disabled');
+    } else {
+      $(event.target).closest('.media').find('.js-send').removeClass('disabled');
+    }
+  },
   'click .js-send'(event) {
+    if ($(event.target).hasClass('disabled')) return;
     const titlearea = $.find('#new_forum_title')[0];
     const textarea = $.find('#new_forum_text')[0];
     if (!titlearea.value) {
@@ -19,7 +28,7 @@ Template.New_forum_topic.events({
       category: 'forum',
       title: titlearea.value,
       text: textarea.value,
-    }, onSuccess(function (res) { textarea.value = ''; titlearea.value = ''; })
+    }, onSuccess(function (res) { textarea.value = ''; titlearea.value = ''; $(event.target).addClass('disabled'); })
     );
   },
 });
