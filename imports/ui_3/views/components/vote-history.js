ce2e4bf0e80a5afb352acea6db6c58f5fe62f6f1
kmildi <keleti.ildi@gmail.com>
2019-02-25 12:37:53 +0100

    Fix translation of fields and values in revision (vote-history)

--- imports/ui_3/views/components/vote-history.js
+++ imports/ui_3/views/components/vote-history.js
@@ -1,24 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
-import { $ } from 'meteor/jquery';
-
-import { moment } from 'meteor/momentjs:moment';
-import { TimeSync } from 'meteor/mizzao:timesync';
 
 import { __ } from '/imports/localization/i18n.js';
-import { onSuccess, handleError } from '/imports/ui_3/lib/errors.js';
-import { Topics } from '/imports/api/topics/topics.js';
 
 import './vote-history.html';
 
-
 Template.Vote_history.helpers({
-  revision() {
-    return this.revision;
+  revision(doc) {
+    return doc.revision;
   },
-  sortedRevisions() {
-    return _.sortBy(this.revision, 'time').reverse();
+  sortedRevisions(doc) {
+    return _.sortBy(doc.revision, 'time').reverse();
+  },
+  translateFieldName(schemaName, field) {
+    const fieldName = schemaName + '.' + field + '.label';
+    return `"${__(fieldName)}"`;
+  },
+  translateFieldValue(schemaName, field, value) {
+    const schemaFieldValue = schemaName + '.' + field + '.' + value;
+    if (__(schemaFieldValue) !== schemaFieldValue) return __(schemaFieldValue);
+    return __(value);
   },
 
 });
