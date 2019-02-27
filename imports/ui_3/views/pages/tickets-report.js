21ec515580b1b4b257214498ac4871fb37c74663
TMSBK <tamas.bako@protonmail.com>
2019-02-27 18:40:12 +0100

    Ticket status filters

--- imports/ui_3/views/pages/tickets-report.js
+++ imports/ui_3/views/pages/tickets-report.js
@@ -5,6 +5,7 @@ import { $ } from 'meteor/jquery';
 import { moment } from 'meteor/momentjs:moment';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { datatables_i18n } from 'meteor/ephemer:reactive-datatables';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Topics } from '/imports/api/topics/topics.js';
 import { ticketsSchema } from '/imports/api/topics/tickets/tickets.js';
@@ -17,6 +18,8 @@ import '/imports/ui_3/views/blocks/chopped.js';
 import './tickets-report.html';
 
 Template.Tickets_report.onCreated(function () {
+  this.ticketStatus = new ReactiveDict();
+  this.ticketStatus.set('status', false);
 });
 
 Template.Tickets_report.helpers({
@@ -31,6 +34,8 @@ Template.Tickets_report.helpers({
   },
   tickets() {
     const communityId = Session.get('activeCommunityId');
+    const status = Template.instance().ticketStatus.get('status');
+    if (status) return Topics.find({ communityId, 'ticket.status': status, category: 'ticket' }, { sort: { createdAt: -1 } });
     return Topics.find({ communityId, category: 'ticket' }, { sort: { createdAt: -1 } });
   },
   recentTickets() {
@@ -89,5 +94,16 @@ Template.Tickets_report.events({
   'click .js-delete'(event) {
     const id = $(event.target).data('id');
     deleteTicketConfirmAndCallModal(id);
+  },
+  'click .js-status-filter'(event, instance) {
+    const status = $(event.target).data('value');
+    if (status === 'cancel') {
+      instance.ticketStatus.set('status', false);
+      $('.js-status-filter').removeClass('js-status-border');
+    } else {
+      instance.ticketStatus.set('status', status);
+      $('.js-status-filter').removeClass('js-status-border');
+      $(event.target).addClass('js-status-border');
+    }
   },
 });
