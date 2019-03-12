ee2d292ea6aabeb3deefcce2dc628dc2fae8d947
droka <daniel.roka@tutanota.com>
2019-03-12 17:01:20 +0100

    Show the user his own parcel's transactions

--- imports/api/transactions/publications.js
+++ imports/api/transactions/publications.js
@@ -2,12 +2,11 @@
 
 import { Meteor } from 'meteor/meteor';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-import { Transactions } from './transactions.js';
-import { Breakdowns } from './breakdowns/breakdowns.js';
+import { _ } from 'meteor/underscore';
+
 import { ChartOfAccounts } from '/imports/api/transactions/breakdowns/chart-of-accounts.js';
 import { Localizer } from '/imports/api/transactions/breakdowns/localizer.js';
-// import { Txs } from './txs.js';
-// import { TxDefs } from './tx-defs.js';
+import { Transactions } from './transactions.js';
 
 Meteor.publish('transactions.byId', function transactionsInCommunity(params) {
   new SimpleSchema({
@@ -35,7 +34,11 @@ Meteor.publish('transactions.byAccount', function transactionsInCommunity(params
 
   const user = Meteor.users.findOneOrNull(this.userId);
   if (!user.hasPermission('transactions.inCommunity', communityId)) {
-    return this.ready();
+    // then he can only see his own parcels' transactions
+    const parcelCodes = user.ownedParcels(communityId).map(p => Localizer.parcelRef2code(p.ref));
+    if (!localizer || !_.contains(parcelCodes, localizer)) {
+      return this.ready();
+    }
   }
   if ((account && localizer) || (!account && !localizer)) {
     throw new Meteor.Error('invalid subscription');
