1b9d8d05546a18520669bdd840bb79edbf54d867
droka <daniel.roka@tutanota.com>
2019-03-30 15:23:44 +0100

    New (failing) test cases for name pre and post fix

--- imports/utils/compare-names.js
+++ imports/utils/compare-names.js
@@ -2,48 +2,56 @@ import { debugAssert } from '/imports/utils/assert.js';
 import { deaccentLowerCase } from '/imports/api/utils.js';
 
 export function compareNames(param1, param2) {
-    debugAssert((typeof param1 === 'object'), 'compareNames param1 must be an object (firstName && lastName or name).');
-    debugAssert(((param1.firstName && param1.lastName) || param1.name), 'firstName and lastName or name is required at compareNames.') 
-  
-    if ((param1.firstName && param1.lastName) && (param2.firstName && param2.lastName)) {
-      if ((param1.firstName === param2.firstName) && (param1.lastName === param2.lastName)) return 'equal';
-      if (param1.firstName.localeCompare(param2.firstName, "hu", { sensitivity: 'base'}) === 0 &&  
-          param1.lastName.localeCompare(param2.lastName, "hu", { sensitivity: 'base'}) === 0) return 'analog';
-      return 'different'
-    };
-    if (((param1.firstName && param1.lastName) && param2.name) || (param1.name && (param2.firstName && param2.lastName))) {
-      const firstName = param1.firstName || param2.firstName;
-      const lastName = param1.lastName || param2.lastName;
-      const name = param1.name || param2.name;
-      const fullNameHu = lastName + ' ' + firstName;
-      const fullNameEn = firstName + ' ' + lastName;
-      if ((fullNameHu === name) || (fullNameEn === name)) return 'equal';
-      if ((fullNameHu.localeCompare(name, "hu", { sensitivity: 'base'}) === 0) || 
-        (fullNameEn.localeCompare(name, "hu", { sensitivity: 'base'}) === 0)) return 'analog';
-      return 'different';
-    };
-    if (param1.name && param2.name) {
-      if (param1.name === param2.name) return 'equal';
-      if (param1.name.localeCompare(param2.name, "hu", { sensitivity: 'base'}) === 0) return 'analog';
+  debugAssert((typeof param1 === 'object'), 'compareNames param1 must be an object (firstName && lastName or name).');
+  debugAssert(((param1.firstName && param1.lastName) || param1.name), 'firstName and lastName or name is required at compareNames.');
+
+  if ((param1.firstName && param1.lastName) && (param2.firstName && param2.lastName)) {
+    if ((param1.firstName === param2.firstName) && (param1.lastName === param2.lastName)) return 'equal';
+    if (param1.firstName.localeCompare(param2.firstName, 'hu', { sensitivity: 'base' }) === 0 &&
+        param1.lastName.localeCompare(param2.lastName, 'hu', { sensitivity: 'base' }) === 0) return 'analog';
+    return 'different';
+  }
+  if (((param1.firstName && param1.lastName) && param2.name) || (param1.name && (param2.firstName && param2.lastName))) {
+    const firstName = param1.firstName || param2.firstName;
+    const lastName = param1.lastName || param2.lastName;
+    const name = param1.name || param2.name;
+    const fullNameHu = lastName + ' ' + firstName;
+    const fullNameEn = firstName + ' ' + lastName;
+    if ((fullNameHu === name) || (fullNameEn === name)) return 'equal';
+    if ((fullNameHu.localeCompare(name, 'hu', { sensitivity: 'base' }) === 0) ||
+      (fullNameEn.localeCompare(name, 'hu', { sensitivity: 'base' }) === 0)) return 'analog';
+    return 'different';
+  }
+  if (param1.name && param2.name) {
+    if (param1.name === param2.name) return 'equal';
+    if (param1.name.localeCompare(param2.name, 'hu', { sensitivity: 'base' }) === 0) return 'analog';
+    return 'different';
+  }
+  if (typeof param2 === 'string') {
+    if (param1.firstName && param1.lastName) {
+      if (param2.includes(param1.firstName) && param2.includes(param1.lastName)) return 'includes';
+      const normFirstName = deaccentLowerCase(param1.firstName);
+      const normLastName = deaccentLowerCase(param1.lastName);
+      const normString = deaccentLowerCase(param2);
+      if (normString.includes(normFirstName) && normString.includes(normLastName)) return 'includes-analog';
       return 'different';
     }
-    if (typeof param2 === 'string') {
-      if (param1.firstName && param1.lastName){
-        if (param2.includes(param1.firstName) && param2.includes(param1.lastName)) return 'includes';
-        const normFirstName = deaccentLowerCase(param1.firstName);
-        const normLastName = deaccentLowerCase(param1.lastName);
-        const normString = deaccentLowerCase(param2);
-        if (normString.includes(normFirstName) && normString.includes(normLastName)) return 'includes-analog';
-        return 'different';
-      }
-      if (param1.name) {
-        if (param2.includes(param1.name)) return 'includes';
-        const normName = deaccentLowerCase(param1.name);
-        const normString = deaccentLowerCase(param2);
-        if (normString.includes(normName)) return 'includes-analog';
-        return 'different';
-      }
+    if (param1.name) {
+      if (param2.includes(param1.name)) return 'includes';
+      const normName = deaccentLowerCase(param1.name);
+      const normString = deaccentLowerCase(param2);
+      if (normString.includes(normName)) return 'includes-analog';
+      return 'different';
     }
-    return;
-  };
-  
+  }
+  debugAssert(false, 'Should never get here');
+  return undefined;
+}
+
+export function namesMatchExact(param1, param2) {
+  return compareNames(param1, param2) === 'equal';
+}
+
+export function namesMatch(param1, param2) {
+  return compareNames(param1, param2) !== 'different';
+}
