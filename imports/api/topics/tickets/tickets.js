f0e838587037387a7e80d67280d4dbb8a47df229
TMSBK <frguitar@gmail.com>
2019-04-01 13:06:53 +0200

    Fix merge conflicts

--- imports/api/topics/tickets/tickets.js
+++ imports/api/topics/tickets/tickets.js
@@ -3,44 +3,6 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { autoformOptions } from '/imports/utils/autoform.js';
 import { _ } from 'meteor/underscore';
 
-<<<<<<< HEAD
-import { Topics } from '../topics.js';
-
-Topics.ticketCategoryValues = ['building', 'garden', 'service'];
-Topics.urgencyValues = ['high', 'normal', 'low'];
-Topics.urgencyColors = {
-  high: 'danger',
-  normal: 'warning',
-  low: 'primary',
-};
-Topics.statusValues = ['reported', 'confirmed', 'progressing', 'finished', 'checked', 'closed', 'deleted'];
-Topics.statusColors = {
-  reported: 'warning',
-  confirmed: 'info',
-  progressing: 'info',
-  finished: 'primary',
-  checked: 'primary',
-  closed: 'default',
-  deleted: 'danger',
-};
-
-Topics.taskStatusValues = ['scheduled', 'progressing', 'finished', 'checked', 'closed', 'deleted'];
-Topics.taskStatusColors = {
-  scheduled: 'warning',
-  progressing: 'info',
-  finished: 'primary',
-  checked: 'primary',
-  closed: 'default',
-  deleted: 'danger',
-};
-
-Topics.columns = ['Status', 'Title', 'Reported by', 'Reported at', 'Type'];
-
-const TicketsExtensionSchema = new SimpleSchema({
-  category: { type: String, allowedValues: Topics.ticketCategoryValues, autoform: autoformOptions(Topics.ticketCategoryValues, 'schemaTickets.ticket.category.'), optional: true },
-  urgency: { type: String, allowedValues: Topics.urgencyValues, autoform: autoformOptions(Topics.urgencyValues, 'schemaTickets.ticket.urgency.'), optional: true },
-  status: { type: String, allowedValues: Topics.statusValues, autoform: autoformOptions(Topics.statusValues, 'schemaTickets.ticket.status.') },
-=======
 import { Topics } from '/imports/api/topics/topics.js';
 import { TicketUrgencyValues, TicketStatusNames, TicketTypeNames } from '/imports/api/topics/tickets/ticket-status.js';
 
@@ -62,7 +24,6 @@ Tickets.extensionSchema = new SimpleSchema({
   actualStart: { type: Date, optional: true },
   actualFinish: { type: Date, optional: true },
   actualContinue: { type: Date, optional: true },
->>>>>>> upstream/ticket_statuses
 });
 
 Tickets.modifiableFields = ['ticket.category', 'ticket.urgency'];
