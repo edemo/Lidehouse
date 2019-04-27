b6a43ac74463d03473fa0f03ad636b9ba53ffc44
kmildi <keleti.ildi@gmail.com>
2019-04-27 17:47:01 +0200

    Vote closes soon email notification

--- imports/email/notifications.js
+++ imports/email/notifications.js
@@ -2,6 +2,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Mailer } from 'meteor/lookback:emails';
 import { TAPi18n } from 'meteor/tap:i18n';
+import { moment } from 'meteor/momentjs:moment';
 import { Topics } from '/imports/api/topics/topics.js';
 import { updateMyLastSeen } from '/imports/api/users/methods.js';
 
@@ -31,4 +32,33 @@ export function processNotifications(frequency) {
   const usersToBeNotified = Meteor.users.find({ 'settings.notiFrequency': frequency });
   // console.log(usersToBeNotified.fetch());
   usersToBeNotified.forEach(user => sendNotifications(user));
+}
+
+export function sendVoteexpiresNoti() {
+  const users = Meteor.users.find({ 'settings.notiFrequency': { $ne: 'never' } });
+  users.forEach((user) => {
+    user.communities().forEach((community) => {
+      const userVoteIndirect = 'voteCastsIndirect.' + user._id;
+      const votes = Topics.find({
+        communityId: community._id,
+        category: 'vote',
+        closed: false,
+        'vote.closesAt': { $gte: moment().add(4, 'day').toDate(), $lt: moment().add(5, 'day').toDate() },
+        [userVoteIndirect]: { $exists: false },
+      }).fetch();
+      if (votes.length > 0) {
+        Mailer.send({
+          to: user.getPrimaryEmail(),
+          bcc: 'Honline <noreply@honline.hu>',
+          subject: TAPi18n.__('email.NotificationTitle', { name: community.name }, user.settings.language),
+          template: 'Voteexpires_Email',
+          data: {
+            userId: user._id,
+            communityId: community._id,
+            alertColor: 'alert-warning',
+          },
+        });
+      }
+    });
+  });
 }
---
+++ imports/email/voteexpires-email.js
@@ -0,0 +1,41 @@
+import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { FlowRouterHelpers } from 'meteor/arillo:flow-router-helpers';
+import { moment } from 'meteor/momentjs:moment';
+import { Topics } from '/imports/api/topics/topics.js';
+import '/imports/api/users/users.js';
+
+export const Voteexpires_Email = {
+  path: 'email/voteexpires-email.html',    // Relative to the 'private' dir.
+  // scss: 'email/style.css',             // Mail specific SCSS.
+
+  helpers: {
+    curb(text, chars) {
+      if (text.length < chars) return text;
+      return text.substr(0, chars) + `... [${TAPi18n.__('see full text with View button', {}, Meteor.users.findOne(this.userId).settings.language)}]`;
+    },
+    topicUrlFor(vote) {
+      return FlowRouterHelpers.urlFor('Topic.show', { _tid: vote._id });
+    },
+    votes() {
+      const userVoteIndirect = 'voteCastsIndirect.' + this.userId;
+      const votes = Topics.find({
+        communityId: this.communityId,
+        category: 'vote',
+        closed: false,
+        'vote.closesAt': { $gte: moment().add(4, 'day').toDate(), $lt: moment().add(5, 'day').toDate() },
+        [userVoteIndirect]: { $exists: false },
+      });
+      return votes;
+    },
+  },
+
+  route: {
+    path: '/voteexpires-email/:uid/:cid',
+    data: params => ({
+      userId: params.uid,
+      communityId: params.cid,
+      alertColor: 'alert-warning',
+    }),
+  },
+};
