93505b06a68f73cee66779f9b928deeeeca94e41
droka <daniel.roka@tutanota.com>
2019-06-03 11:52:27 +0200

    viewmodel simplifies hideable template

--- imports/ui_3/views/blocks/hideable.js
+++ imports/ui_3/views/blocks/hideable.js
@@ -1,1 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
+import { Template } from 'meteor/templating';
+
 import './hideable.html';
+
+Template.hideable.viewmodel({
+  hider: null,
+  onCreated(instance) {
+    const userId = Meteor.userId();
+    const communityId = Session.get('activeCommunityId');
+    this.hider(instance.data.hiddenBy(userId, communityId));
+  },
+  events: {
+    'click .js-show'(event, instance) {
+      this.hider(null);
+    },
+  },
+});
