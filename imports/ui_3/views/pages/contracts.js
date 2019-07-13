8b7b5eca35f1761f5f6a8eff6f7d67358b5aa7c8
droka <daniel.roka@tutanota.com>
2019-07-13 13:40:50 +0200

    New worksheets button on contracts page

--- imports/ui_3/views/pages/contracts.js
+++ imports/ui_3/views/pages/contracts.js
@@ -1,8 +1,14 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { $ } from 'meteor/jquery';
+
+import { onSuccess, handleError, displayMessage, displayError } from '/imports/ui_3/lib/errors.js';
 import { Contracts } from '/imports/api/contracts/contracts.js';
+import { Topics } from '/imports/api/topics/topics.js';
+import { Tickets } from '/imports/api/topics/tickets/tickets.js';
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import '/imports/ui_3/views/components/ticket-list.html';
 import './contracts.html';
@@ -30,7 +36,7 @@ Template.Contracts.events({
       meteormethod: 'contracts.insert',
     });
   },
-  'click .js-edit'(event) {
+  'click .contract-details .js-edit'(event) {
     const id = $(event.target).data('id');
     Modal.show('Autoform_edit', {
       id: 'af.contract.update',
@@ -41,11 +47,27 @@ Template.Contracts.events({
       singleMethodArgument: true,
     });
   },
-  'click .js-delete'(event) {
+  'click .contract-details .js-delete'(event) {
     const id = $(event.target).data('id');
     Modal.confirmAndCall(Contracts.methods.remove, { _id: id }, {
       action: 'delete contract',
       message: 'This will not delete worksheets',
+    });
+  },
+  'click .contract-details .js-add'(event) {
+    const id = $(event.target).data('id');
+    Session.set('activeContractId', id);
+    const schemaWithMoreDates = new SimpleSchema([Tickets.schema, {
+      moreDates: { type: [Date], optional: true },
+    }]);
+    schemaWithMoreDates.i18n('schemaTickets');
+    Modal.show('Autoform_edit', {
+      id: 'af.worksheets.insert',
+      schema: schemaWithMoreDates,
+      fields: ['title', 'text',
+        'ticket.type', 'ticket.localizer', 'ticket.chargeType', 'ticket.expectedStart',
+        'moreDates',
+      ],
     });
   },
 });
@@ -56,5 +78,43 @@ AutoForm.addHooks('af.contract.insert', {
   formToDoc(doc) {
     doc.communityId = Session.get('activeCommunityId');
     return doc;
+  },
+});
+
+AutoForm.addModalHooks('af.worksheets.insert');
+AutoForm.addHooks('af.worksheets.insert', {
+  formToDoc(doc) {
+    doc.communityId = Session.get('activeCommunityId');
+    doc.userId = Meteor.userId();
+    doc.category = 'ticket';
+    doc.status = Tickets.workflows[doc.ticket.type].start[0].name;
+    doc.ticket.contractId = Session.get('activeContractId');
+    doc.moreDates = doc.moreDates || [];
+    return doc;
+  },
+  onSubmit(doc) {
+    AutoForm.validateForm('af.worksheets.insert');
+    const afContext = this;
+    const results = [];
+    function insert(ticket) {
+      Topics.methods.insert.call(ticket, function handler(err, res) {
+        if (err) {
+          displayError(err);
+          afContext.done(err);
+          return;
+        }
+        results.push(res);
+        if (results.length === doc.moreDates.length + 1) afContext.done(null, results);
+      });
+    }
+    insert(doc);
+    doc.moreDates.forEach((date) => {
+      doc.ticket.expectedStart = date;
+      insert(doc);
+    });
+    return false;
+  },
+  onSuccess(formType, result) {
+    Session.set('activeContractId');  // clear it
   },
 });
