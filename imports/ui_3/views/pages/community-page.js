1988cc37d75bf4546d5eee55c6fbb186f70b0394
droka <daniel.roka@tutanota.com>
2019-07-30 13:07:48 +0200

    put admin from front to the end of listing

--- imports/ui_3/views/pages/community-page.js
+++ imports/ui_3/views/pages/community-page.js
@@ -111,7 +111,9 @@ Template.Community_page.viewmodel({
     return Memberships.find({ communityId, active: true, role: { $in: nonLeaderRoles } }, { sort: { createdAt: 1 } }).fetch();
   },
   officers() {
-    return this.leaders().concat(this.nonLeaders());
+    const officers = this.leaders().concat(this.nonLeaders());
+    officers.push(officers.shift());  // put admin from front to the end
+    return officers;
   },
   ownerships() {
     const communityId = this.communityId();
