eeb0cbc3fe81232bb21ae4addb59dd8065799cbb
TMSBK <frguitar@gmail.com>
2019-08-23 16:27:10 +0200

    Refactored Badge

--- imports/ui_3/views/components/badge.js
+++ imports/ui_3/views/components/badge.js
@@ -2,11 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { Topics } from '/imports/api/topics/topics.js';
-import '/imports/api/users/users.js';
+import { Parcels } from '/imports/api/parcels/parcels.js';
+import { Memberships } from '/imports/api/memberships/memberships.js';
 
+import '/imports/api/users/users.js';
 import './badge.html';
 
-Template.Badge.helpers({
+Template.Badge.viewmodel({
+  selected(helper, category) {
+    return this[helper](category);
+  },
   countTasks(category) {
     const communityId = Session.get('activeCommunityId');
     let count = 0;
@@ -16,5 +21,21 @@ Template.Badge.helpers({
       if (User.hasPermission(`${category}.statusChangeTo.${t.status}.leave`)) count += 1;
     });
     return count;
+  },
+  countNotifications(category) {
+    const communityId = Session.get('activeCommunityId');
+    let count = 0;
+    const topics = Topics.find({ communityId, category });
+    topics.map(t => {
+      const userId = Meteor.userId();
+      count += t.needsAttention(userId, Meteor.users.SEEN_BY.EYES);
+    });
+    return count;
+  },
+  countUnapprovedEntities() {
+    const communityId = Session.get('activeCommunityId');
+    const unapprovedParcelCount = Parcels.find({ communityId, approved: false }).count();
+    const unapprovedMembershipCount = Memberships.find({ communityId, approved: false }).count();
+    return unapprovedParcelCount + unapprovedMembershipCount;
   },
 });
