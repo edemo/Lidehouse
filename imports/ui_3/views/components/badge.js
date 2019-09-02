4a597c4bbfeacec69c6eff657ed6361bc8687661
TMSBK <frguitar@gmail.com>
2019-09-02 14:29:25 +0200

    Waiting for parent subsRdy, removed 'this' as ctx

--- imports/ui_3/views/components/badge.js
+++ imports/ui_3/views/components/badge.js
@@ -1,1 +1,10 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
 import './badge.html';
+
+Template.Badge.viewmodel({
+  parentSubscriptionsReady() {
+    return Template.instance().parent(1).subscriptionsReady();
+  },
+});
