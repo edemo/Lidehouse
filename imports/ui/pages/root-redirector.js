d8be7443af6745d8f792c5f097e21f55918ec501
kmildi <keleti.ildi@gmail.com>
2019-09-24 18:03:58 +0200

    Change layout of root-redirector - fix intropage navbar position

--- imports/ui/pages/root-redirector.js
+++ imports/ui/pages/root-redirector.js
@@ -9,6 +9,17 @@ Template.app_rootRedirector.onCreated(() => {
   if (Meteor.userId()) {
     Template.instance().subscribe('memberships.ofUser', { userId: Meteor.userId() });
     Template.instance().autorun(() => {
+      const activeCommunityId = Session.get('activeCommunityId');
+      const user = Meteor.user();
+      if (user && (!activeCommunityId || !user.isInCommunity(activeCommunityId))) {
+        const communities = user.communities();
+        if (communities.count() > 0) {
+          const activeCommunity = communities.fetch()[0];
+          Session.set('activeCommunityId', activeCommunity._id);
+        }
+      }
+    });
+    Template.instance().autorun(() => {
       if (Template.instance().subscriptionsReady()) {
         if (Session.get('activeCommunityId')) {
           FlowRouter.go('Board');
@@ -18,10 +29,14 @@ Template.app_rootRedirector.onCreated(() => {
       }
     });
   } else {
-      if (Meteor.settings.public.communityId) {
+    if (Meteor.settings.public.communityId) {
+      Meteor.setTimeout(() => {
         FlowRouter.go('Community page', { _cid: Meteor.settings.public.communityId });
-      } else {
+      });
+    } else {
+      Meteor.setTimeout(() => {
         FlowRouter.go('App intro');
-      }
+      });
+    }
   }
 });
--- imports/ui_3/views/brand/intro-page.js
+++ imports/ui_3/views/brand/intro-page.js
@@ -7,7 +7,6 @@ import './intro-page.html';
 Template.Intro_page.onRendered(function(){
 
     $('body').addClass('landing-page');
-    $('body').removeClass('fixed-nav').removeClass('fixed-nav-basic');  // added by the main navbar
     $('body').attr('id', 'page-top');
     $('body').scrollspy({
         target: '.navbar-fixed-top',
