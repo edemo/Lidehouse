531e95d9c371ccce0aff2df44ce6c0cd044f3f26
droka <daniel.roka@tutanota.com>
2019-10-19 14:56:39 +0200

    Fix Payments publication

--- imports/ui_3/views/pages/bill-show.js
+++ imports/ui_3/views/pages/bill-show.js
@@ -35,11 +35,6 @@ Template.Bill_show.helpers({
   actions() {
     return Bills.actions;
   },
-  getPayments(bill) {
-     const res = bill.getPayments();
-     debugger;
-     return res;
-  },
 });
 
 function runInNewModal(toRun) {
