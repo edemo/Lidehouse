bd3d0502dac00db42197c547600e123cc584c579
droka <daniel.roka@tutanota.com>
2019-11-30 17:59:18 +0100

    can get rid of datatable render buttons

--- imports/ui_3/lib/datatable-renderers.js
+++ imports/ui_3/lib/datatable-renderers.js
@@ -2,9 +2,7 @@ import { moment } from 'meteor/momentjs:moment';
 import { numeral } from 'meteor/numeral:numeral';
 import { __ } from '/imports/localization/i18n.js';
 import { Topics } from '/imports/api/topics/topics.js';
-import { Tickets } from '/imports/api/topics/tickets/tickets.js';
 import { $ } from 'meteor/jquery';
-import { Localizer } from '/imports/api/transactions/breakdowns/localizer.js';
 
 export const Render = {
   translate(cellData, renderType, currentRow) {
@@ -45,46 +43,6 @@ export const Render = {
     html += `<a href="#view-target">`;
     html += `<button data-id=${cellData} class="btn btn-white btn-xs js-view" title=${__('view')}><i class="fa fa-eye"></i></button>`;
     html += `</a>`;
-    return html;
-  },
-  buttonEdit(cellData, renderType, currentRow) {
-    const html = `<button data-id=${cellData} class="btn btn-white btn-xs js-edit" title=${__('edit')}><i class="fa fa-pencil"></i></button>`;
-    return html;
-  },
-  buttonDelete(cellData, renderType, currentRow) {
-    const html = `<button data-id=${cellData} class="btn btn-white btn-xs js-delete" title=${__('delete')}><i class="fa fa-trash"></i></button>`;
-    return html;
-  },
-  buttonGroup(buttonRenderers) {
-    return function groupRenderer(cellData, renderType, currentRow) {
-      let html = '<div class="btn-group">';
-      buttonRenderers.forEach((renderer) => {
-        html += renderer(cellData, renderType, currentRow);
-      });
-      html += '</div>';
-      return html;
-    };
-  },
-  buttonStatusChange(cellData) {
-    const thisTopic = Topics.findOne(cellData);
-    let html = `<div class="dropdown pull-left">
-                  <a href="" data-toggle="dropdown" class="dropdown-toggle btn btn-xs btn-white" title="${__('statusChange')}"}>
-                    <i class="fa fa-cogs"></i> 
-                    <b class="caret"></b>
-                  </a>
-                  <ul class="slim-menu dropdown-menu animated fadeInDown m-t-xs">`;
-    thisTopic.possibleNextStatuses().forEach((status) => {
-      html += `<li>
-                <a href="" class="js-status-change" data-id="${cellData}" data-status="${status.name}">
-                  ${__('schemaTopics.status.' + status.name)}
-                </a>
-              </li>`;
-    });
-    html += '</ul></div>';
-    return html;
-  },
-  buttonStatusUpdate(cellData) {
-    const html = `<button data-id=${cellData} class="btn btn-white btn-xs js-status-update" title=${__('statusUpdate')}><i class="fa fa-edit"></i></button>`;
     return html;
   },
   joinOccupants(occupants) {
