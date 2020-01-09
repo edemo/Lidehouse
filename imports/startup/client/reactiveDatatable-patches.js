12982fc661001a7be5f0859334af47f9dfa928d3
TMSBK <frguitar@gmail.com>
2020-01-09 14:06:10 +0100

    Refactor user-select event handler

--- imports/startup/client/reactiveDatatable-patches.js
+++ imports/startup/client/reactiveDatatable-patches.js
@@ -1,12 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
 
 // This code helps to stop event bubbling on dataTable buttons, so it doesn't select the row after button click
 // Classic event.stopPropagation() on button doesn't work, cause the table immediately gets the click event
 // ref: https://datatables.net/reference/event/user-select
 Template.ReactiveDatatable.onRendered(function () {
-  const table = this.$('.dataTable');
-  table.DataTable().on('user-select', function( e, dt, type, cell, originalEvent) {
-    if (originalEvent.target.classList.contains('btn') || originalEvent.target.nodeName === 'I') e.preventDefault();
+  this.$('.dataTable').DataTable().on('user-select', function( e, dt, type, cell, originalEvent) {
+    if ($(originalEvent.target).closest('.btn').length) e.preventDefault();
   });
 });
