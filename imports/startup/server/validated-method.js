9aacd122e9df2bce6dc473dc58badbe5f0a037a0
droka <daniel.roka@tutanota.com>
2020-02-07 22:49:50 +0100

    Log method times

--- imports/startup/server/validated-method.js
+++ imports/startup/server/validated-method.js
@@ -1,11 +1,15 @@
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { CollectionHooks } from 'meteor/matb33:collection-hooks';
+import { _ } from 'meteor/underscore';
 
 ValidatedMethod.prototype._mdgExecute = ValidatedMethod.prototype._execute;
 
 ValidatedMethod.prototype._execute = function (...args) {
   CollectionHooks.defaultUserId = args[0].userId;
+  const start = Date.now();
   const result = this._mdgExecute(...args);
+  const finish = Date.now();
+  console.log('Method', this.name, finish - start, 'ms', args[0].userId);
   CollectionHooks.defaultUserId = undefined;
   return result;
 };
