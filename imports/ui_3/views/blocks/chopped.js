5c327d76ac2d11a3cada89ee8480ea7c8194b5ad
TMSBK <frguitar@gmail.com>
2020-04-08 11:51:09 +0200

    Refactoring chopped, fixing sanitize bug

--- imports/ui_3/views/blocks/chopped.js
+++ imports/ui_3/views/blocks/chopped.js
@@ -7,16 +7,18 @@ const CHOP_AT_CHARS = 600;
 
 Template.Chopped.viewmodel({
   showmore: false,
-  choppedText(ctx) {
+  choppedText() {
+    const td = this.templateInstance.data;
     let text;
-    const chars = isNaN(ctx.chars) ? CHOP_AT_CHARS : ctx.chars; // Bypassing Blaze auto last params in helper if chars is not set
-    if (ctx.text.length <= chars || this.showmore()) text = ctx.text;
-    else text = ctx.text.substr(0, chars) + '... ';
-    return ctx.markdown ? sanitizeHtml(marked(text)) : text;
+    const chars = isNaN(td.chars) ? CHOP_AT_CHARS : td.chars; // Bypassing Blaze auto last params in helper if chars is not set
+    if (td.text.length <= chars || this.showmore()) text = td.text;
+    else text = td.text.substr(0, chars) + '... ';
+    return td.markdown ? marked(sanitizeHtml(text)) : text;
   },
-  isChopped(ctx) {
-    const chars = isNaN(ctx.chars) ? CHOP_AT_CHARS : ctx.chars; // Bypassing Blaze auto last params in helper if chars is not set
-    return (!this.showmore() && ctx.text && ctx.text.length > chars);
+  isChopped() {
+    const td = this.templateInstance.data;
+    const chars = isNaN(td.chars) ? CHOP_AT_CHARS : td.chars; // Bypassing Blaze auto last params in helper if chars is not set
+    return (!this.showmore() && td.text && td.text.length > chars);
   },
 });
 
