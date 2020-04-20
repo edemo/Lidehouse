93b8747faf27cddbb52b97baae5428377a6856d7
TMSBK <frguitar@gmail.com>
2020-04-20 11:48:35 +0200

    Removed unnecessary dependency

--- imports/ui_3/views/common/page-heading.js
+++ imports/ui_3/views/common/page-heading.js
@@ -1,7 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Transactions } from '/imports/api/transactions/transactions.js';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { print } from '/imports/ui_3/views/components/print-action.js';
 import './page-heading.html';
 
 Template.Page_heading.helpers({
