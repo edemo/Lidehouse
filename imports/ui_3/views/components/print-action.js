cab46fa3cf99a847f355261bd2d8e4963c45fbfc
TMSBK <frguitar@gmail.com>
2020-04-27 12:08:19 +0200

    Printable helper

--- imports/ui_3/views/components/print-action.js
+++ imports/ui_3/views/components/print-action.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { debugAssert } from '/imports/utils/assert.js';
 import { ModalStack } from '/imports/ui_3/lib/modal-stack.js';
+import { Transactions } from '/imports/api/transactions/transactions.js';
 import { $ } from 'meteor/jquery';
 
 Template.registerHelper('print', function print() {
@@ -19,4 +21,18 @@ Template.registerHelper('print', function print() {
       } else window.print();
     },
   };
+});
+
+Template.registerHelper('printable', function printable() {
+  const routeName = FlowRouter.getRouteName();
+  if (routeName === 'Transaction show') {
+    const _txid = FlowRouter.getParam('_txid');
+    const tx = Transactions.findOne(_txid);
+    return tx && tx.category === 'bill';
+  }
+  if (ModalStack.active() && this.doc) {
+    const tx = Transactions.findOne(this.doc._id);
+    return tx && tx.category === 'bill';
+  }
+  return false;
 });
