59547bebae17ce81075a96105fa8eb44119aabcd
droka <daniel.roka@tutanota.com>
2020-05-02 12:29:33 +0200

    Render whole table header cell with Blaze

--- imports/ui_3/views/components/import-dialog.js
+++ imports/ui_3/views/components/import-dialog.js
@@ -1,11 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
 import { _ } from 'meteor/underscore';
 import { $ } from 'meteor/jquery';
 
-import { __ } from '/imports/localization/i18n.js';
 import './import-dialog.html';
 
 Template.Import_upload.viewmodel({
@@ -29,22 +26,19 @@ Template.Import_preview.viewmodel({
 });
 
 Template.Import_preview.onRendered(function () {
-  const columns = this.data.columns.filter(c => c.key);
-  const knownColumnNames = _.without(_.pluck(columns, 'name'), undefined);
-  this.viewmodel.availableColumns([].concat(knownColumnNames));
+  const columns = this.data.columns.filter(c => c.key); // leave out the sperators, like "PARCELS DATA"
+  const validColumnNames = _.without(_.pluck(columns, 'name'), undefined);
+  this.viewmodel.availableColumns([].concat(validColumnNames));
   const tableElem = this.$('table');
   tableElem.addClass('table dataTable display import-table');
   const headerRow = this.$('tr:first-child');
   headerRow.children().each((i, td) => {
     const tdElem = $(td);
-    if (_.contains(this.viewmodel.availableColumns(), td.innerText)) {
-      tdElem.addClass('bg-primary');
-      this.viewmodel.availableColumns(_.without(this.viewmodel.availableColumns(), td.innerText));
-    } else {
-      tdElem.addClass('bg-danger');
-      tdElem.append('<br>');
-      Blaze.renderWithData(Template.Select_from_columns, { columns: this.viewmodel.availableColumns }, td);
-    }
+    const _columnName = td.innerText;
+    tdElem.empty();
+    Blaze.renderWithData(Template.Import_header_cell,
+      { _columnName, columns: validColumnNames, availableColumns: this.viewmodel.availableColumns }, td
+    );
   });
 });
 
@@ -59,21 +53,33 @@ Template.Import_dialog.viewmodel({
 
 //--------------------------------------------
 
-Template.Select_from_columns.viewmodel({
-  availableColums() {
-    const columns = this.templateInstance.data.columns; // it is a viewmodel reactiveVar
-    return columns();
+Template.Import_header_cell.viewmodel({
+  columnName: '',
+  onCreated(instance) {
+    this.columnName(instance.data._columnName);
+  },
+  autorun() {
+    const data = this.templateInstance.data;
+    if (_.contains(data.availableColumns(), this.columnName())) {
+      data.availableColumns(_.without(data.availableColumns(), this.columnName()));
+    }
+  },
+  isValidColumn() {
+    return _.contains(this.getValidColums(), this.columnName());
+  },
+  getAvailableColums() {
+    return this.templateInstance.data.availableColumns(); // it is a viewmodel var, in the parent (so they share it)
+  },
+  getValidColums() {
+    return this.templateInstance.data.columns;
   },
 });
 
-Template.Select_from_columns.events({
+Template.Import_header_cell.events({
   'change select'(event, instance) {
     const selected = event.target.selectedOptions[0].value;
-    const tdElem = $(event.target).closest('td');
-    tdElem[0].innerText = selected;
-    tdElem.removeClass('bg-danger').addClass('bg-primary');
-    instance.data.columns(_.without(instance.data.columns(), selected));
-    Blaze.remove(instance.view);
+    instance.viewmodel.columnName(selected);
+//    Blaze.remove(instance.view);  should remove somewhere
   },
 });
 
