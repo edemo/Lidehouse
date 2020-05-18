b7c2ba8ae20c4f015682af33d855a081a9d85ed5
droka <daniel.roka@tutanota.com>
2020-05-18 19:03:10 +0200

    Extract chars helper

--- imports/ui_3/views/blocks/chopped.js
+++ imports/ui_3/views/blocks/chopped.js
@@ -6,18 +6,20 @@ const CHOP_AT_CHARS = 600;
 
 Template.Chopped.viewmodel({
   showmore: false,
+  chars() {
+    const chars = this.templateInstance.data.chars;
+    return isNaN(chars) ? CHOP_AT_CHARS : chars; // Bypassing Blaze auto last params in helper if chars is not set
+  },
   choppedText() {
     const td = this.templateInstance.data;
     let text;
-    const chars = isNaN(td.chars) ? CHOP_AT_CHARS : td.chars; // Bypassing Blaze auto last params in helper if chars is not set
-    if (td.text.length <= chars || this.showmore()) text = td.text;
-    else text = td.text.substr(0, chars) + '...';
+    if (td.text.length <= this.chars() || this.showmore()) text = td.text;
+    else text = td.text.substr(0, this.chars()) + '...';
     return td.markdown ? marked.inlineLexer(sanitizeHtml(text), []) : text;
   },
   isChopped() {
     const td = this.templateInstance.data;
-    const chars = isNaN(td.chars) ? CHOP_AT_CHARS : td.chars; // Bypassing Blaze auto last params in helper if chars is not set
-    return (!this.showmore() && td.text && td.text.length > chars);
+    return (!this.showmore() && td.text && td.text.length > this.chars());
   },
 });
 
