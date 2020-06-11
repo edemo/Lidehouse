d82031f4c07cafce08f6ab75da7126b4a96d18ad
droka <daniel.roka@tutanota.com>
2020-06-11 17:38:43 +0200

    Fix parcel-finances subscription

--- imports/api/parcels/tables.js
+++ imports/api/parcels/tables.js
@@ -50,7 +50,7 @@ export function parcelFinancesColumns() {
     { data: 'type', title: __('schemaParcels.type.label') },
     { data: 'occupants()', title: __('occupants'), render: Render.joinOccupants },
     { data: 'withFollowers()', title: __('follower parcels') },
-    { data: 'payerContract().outstanding', title: __('schemaBills.outstanding.label') },
+    { data: 'payerContract()?.outstanding', title: __('schemaBills.outstanding.label') },
     { data: '_id', title: __('Action buttons'), render: Render.actionButtons,
       createdCell: (cell, cellData, rowData) => Blaze.renderWithData(Template.Action_buttons_group,
       { doc: cellData, collection: 'parcels', actions: 'finances,meters', size: 'sm' }, cell),
