13d94bfdaeed3e030da96499783d75e8f07cd03e
droka <daniel.roka@tutanota.com>
2020-07-05 19:52:44 +0200

    Fx stopEnterKey

--- imports/startup/client/autoform-config.js
+++ imports/startup/client/autoform-config.js
@@ -19,10 +19,9 @@ AutoForm.setDefaultTemplateForType('afFormGroup', 'bootstrap3-help');
 // On forms, the enter key should NOT submit the form
 // best would be if Return worked as a TAB btn, but if that cannot be done, let's supress it altogether
 // http://webcheatsheet.com/javascript/disable_enter_key.php
-function stopEnterKey(evt) {
-  const evt = (evt) ? evt : ((event) ? event : null);
-  const node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
-  if ((evt.keyCode == 13) && (node.type == 'text')) return false;
+function stopEnterKey(event) {
+  const node = event.target || (event.srcElement) || null;
+  if ((event.keyCode == 13) && (node.type == 'text' || node.type == 'number')) return false;
 }
 
 Meteor.startup(() => document.onkeypress = stopEnterKey);
