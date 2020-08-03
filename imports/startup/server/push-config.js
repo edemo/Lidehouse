2c0d75d78236422b665fa26b13d58fc0f34aea3a
kmildi <keleti.ildi@gmail.com>
2020-08-03 11:17:36 +0200

    Fix absoluteUrl generation for images

--- imports/startup/server/push-config.js
+++ imports/startup/server/push-config.js
@@ -2,8 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import { Push } from 'meteor/raix:push';
 
 Meteor.startup(function () {
-  console.log('Running with Meteor.settings:', Meteor.settings);
-
   if (Meteor.settings.android && Meteor.settings.public.android) {
     Push.Configure({
       gcm: {
