036b85c882ff662a351ce254058f2e09a63ca6ae
kmildi <keleti.ildi@gmail.com>
2020-09-23 15:34:06 +0200

    Attachment (photo) field comes from behaviour

---
+++ imports/api/behaviours/attachment-field.js
@@ -0,0 +1,59 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { _ } from 'meteor/underscore';
+import { Attachments } from '/imports/api/attachments/attachments.js';
+import { attachmentUpload } from '/imports/utils/autoform.js';
+
+const schema = new SimpleSchema({
+  photo: { type: Array, optional: true },
+  'photo.$': { type: String, optional: true, autoform: attachmentUpload() },
+});
+
+schema.i18n('schemaAttachmentField');
+
+const publicFields = {
+  photo: 1,
+};
+
+const modifiableFields = ['photo'];
+
+const helpers = {
+};
+
+function hooks(collection) {
+  return {
+    after: {
+      insert(userId, doc) {
+        const uploadIds = [];
+        doc.photo?.forEach((path) => {
+          const uploaded = Attachments.findOne({ communityId: doc.communityId, path });
+          if (uploaded) uploadIds.push(uploaded._id);
+        });
+        uploadIds.forEach(id => Attachments.update(id, { $set: { topicId: doc._id } }));
+        return true;
+      },
+      update(userId, doc, fieldNames, modifier, options) {
+        const uploadIds = [];
+        modifier.$set?.photo?.forEach((path) => {
+          const uploaded = Attachments.findOne({ communityId: doc.communityId, path });
+          if (uploaded && !uploaded.topicId) uploadIds.push(uploaded._id);
+        });
+        uploadIds.forEach(id => Attachments.update(id, { $set: { topicId: doc._id } }));
+/*         if (modifier.$unset?.photo) {
+          if (doc._id) Attachments.remove({ communityId: doc.communityId, topicId: doc.id });
+        }); */ // droka:autoform-ufs package removes the uploaded files before submitting the form
+        return true;
+      },
+/*       remove(userId, doc) {
+        if (doc._id) Attachments.remove({ topicId: doc.id });
+        return true;
+      }, */
+    },
+  };
+}
+
+
+export const AttachmentField = { name: 'AttachmentField',
+  schema, publicFields, modifiableFields, helpers, hooks,
+};
