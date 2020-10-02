eaacd4484c63c2540384699992afdf1cc72a1200
kmildi <keleti.ildi@gmail.com>
2020-10-02 16:32:20 +0200

    Image modal for attachments

--- imports/ui_3/views/components/attachments.js
+++ imports/ui_3/views/components/attachments.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
-
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
+import '/imports/ui_3/views/blocks/image.js';
 import './attachments.html';
-
 
 Template.Attachments.helpers({
   byType() {
@@ -15,5 +15,17 @@ Template.Attachments.helpers({
   fileName(path) {
     const name = path.split('/').pop();
     return decodeURI(name);
+  },
+});
+
+Template.Attachments.events({
+  'click .js-show-image'(event, instance) {
+    const url = event.target.getAttribute('src');
+    Modal.show('Modal', {
+      title: (decodeURI(url.split('/').pop())),
+      body: 'Image',
+      bodyContext: { url },
+      size: 'lg',
+    });
   },
 });
