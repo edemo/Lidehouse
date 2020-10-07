17615a3105861ea59058524980a464168d288f4f
kmildi <keleti.ildi@gmail.com>
2020-10-07 18:52:09 +0200

    isImage function to autoform-ufs package

--- imports/ui_3/views/components/attachments.js
+++ imports/ui_3/views/components/attachments.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
+import { isImage } from 'meteor/droka:autoform-ufs/lib/client/af-file-upload.js';
 import '/imports/ui_3/views/blocks/image.js';
 import './attachments.html';
 
@@ -7,7 +8,7 @@ Template.Attachments.helpers({
   byType() {
     const attachments = { images: [], files: [] };
     this.doc.attachments?.forEach((path) => {
-      if ((/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).test(path)) attachments.images.push(path);
+      if (isImage(path)) attachments.images.push(path);
       else attachments.files.push(path);
     });
     return attachments;
