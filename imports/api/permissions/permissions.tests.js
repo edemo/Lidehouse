387d9c4857b9c8c85eef940cc2dcd73626068e40
kmildi <keleti.ildi@gmail.com>
2021-10-15 15:50:27 +0200

    Performance tests

--- imports/api/permissions/permissions.tests.js
+++ imports/api/permissions/permissions.tests.js
@@ -2,6 +2,7 @@
 /* eslint-disable func-names, prefer-arrow-callback */
 
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 /*
 import { Factory } from 'meteor/dburles:factory';
 import { PublicationCollector } from 'meteor/johanbrook:publication-collector';
@@ -10,14 +11,14 @@ import { Random } from 'meteor/random';
 import { _ } from 'meteor/underscore';
 
 import { Roles } from './roles.js';
-import { Permissions } from './permissions.js';
 */
+import { Permissions } from './permissions.js';
 
 if (Meteor.isServer) {
   // eslint-disable-next-line import/no-unresolved
   import './publications.js';
 
-  describe('roles', function () {
+  xdescribe('permissions', function () {
     describe('mutators', function () {
       it('builds correctly from factory', function () {
       });
@@ -27,9 +28,28 @@ if (Meteor.isServer) {
 
     });
 
-    describe('whatever', function () {
-      it('soooo', function () {
+    describe('Performance tests', function () {
+      let PermissionsColl;
+      before(function () {
+        PermissionsColl = new Mongo.Collection('permissions');
+        PermissionsColl._ensureIndex({ name: 1 });
+        for (let i = 10000; i < 10200; i++) {
+          PermissionsColl.insert({ name: i.toString() });
+        }
+      });
 
+      it('searching in Array', function () {
+        const permissionName = 'attachments.remove';
+        for (let i = 0; i < 1000; i++) {
+          Permissions.find(p => p.name === permissionName);
+        }
+      });
+
+      it('searching in Collection', function () {
+        const permissionName = '10148';
+        for (let i = 0; i < 1000; i++) {
+          PermissionsColl.findOne({ name: permissionName });
+        }
       });
     });
 
--- imports/utils/meteor.tests.js
+++ imports/utils/meteor.tests.js
@@ -3,9 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { chai, assert } from 'meteor/practicalmeteor:chai';
 import { _ } from 'meteor/underscore';
+import { PerformanceLogger } from '/imports/utils/performance-logger.js';
 
 if (Meteor.isServer) {
-  describe('meteor functionality', function() {
+  xdescribe('meteor functionality', function() {
     it('foreach iterates on fetched data', function() {
       const Thingys = new Mongo.Collection('thingys');
       Thingys.insert({ counter: 1 });
@@ -23,6 +24,34 @@ if (Meteor.isServer) {
       chai.assert.deepEqual(duringIterationResult, [1, 2, 4]);
       const afterIterationResult = Thingys.find().fetch();
       chai.assert.deepEqual(_.pluck(afterIterationResult, 'counter'), [1, 12, 4, 3]);
+    });
+
+    it('updates faster in one operation', function() {
+      const Puffs = new Mongo.Collection('puffs');
+      const ids = [];
+      for (let i = 0; i < 100; i++) {
+        const id = Puffs.insert({ value: i });
+        ids.push(id);
+      }
+      let puffsInOrder = Puffs.find({}, { sort: { value: 1 } }).fetch();
+      chai.assert.equal(puffsInOrder[0].value, 0);
+      chai.assert.equal(puffsInOrder[99].value, 99);
+
+      PerformanceLogger.startAggregation();
+      ids.forEach(id => {
+        PerformanceLogger.call('update', Puffs.update, Puffs, { _id: id }, { $inc: { value: 1 } }, { multi: true });
+      });
+      PerformanceLogger.stopAggregation();
+      puffsInOrder = Puffs.find({}, { sort: { value: 1 } }).fetch();
+      chai.assert.equal(puffsInOrder[0].value, 1);
+      chai.assert.equal(puffsInOrder[99].value, 100);
+
+      PerformanceLogger.call('Multiupdate', Puffs.update, Puffs, { _id: { $in: ids } }, { $inc: { value: 1 } }, { multi: true });
+      puffsInOrder = Puffs.find({}, { sort: { value: 1 } }).fetch();
+      chai.assert.equal(puffsInOrder[0].value, 2);
+      chai.assert.equal(puffsInOrder[1].value, 3);
+      chai.assert.equal(puffsInOrder[3].value, 5);
+      chai.assert.equal(puffsInOrder[99].value, 101);
     });
   });
 }
