54b04d996c61e9b2e842b013bf09c6a7f7676847
kmildi <keleti.ildi@gmail.com>
2021-11-11 13:12:48 +0100

    NoRetry option to Meteor.apply, error translation

--- imports/startup/client/meteor.js
+++ imports/startup/client/meteor.js
@@ -6,11 +6,16 @@ import { _ } from 'meteor/underscore';
 // Meteor.call(name, [arg1, arg2...], [asyncCallback])
 // Meteor.apply(name, args, [options], [asyncCallback])
 Meteor.call = function callWoRetry(name, ...args) {
-  console.log('callWoRetry', name);
   const options = { noRetry: true };
   let asyncCallback;
   if (typeof _.last(args) === 'function') {
     asyncCallback = args.pop();
   }
   return Meteor.apply(name, args, options, asyncCallback);
+};
+
+Meteor._origApply = Meteor.apply;
+Meteor.apply = function applyWoRetry(name, args, options, asyncCallback) {
+  const optionsWoRetry = _.extend(options, { noRetry: true });
+  return Meteor._origApply(name, args, optionsWoRetry, asyncCallback);
 };
