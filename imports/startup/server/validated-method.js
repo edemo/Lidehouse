dab9ed0c9b4684b2c02942ee634389045d645e09
kmildi <keleti.ildi@gmail.com>
2022-04-12 16:37:29 +0200

    Fix hooks defaultUserId in nested methods

--- imports/startup/server/validated-method.js
+++ imports/startup/server/validated-method.js
@@ -5,8 +5,13 @@ import { PerformanceLogger } from '/imports/utils/performance-logger.js';
 ValidatedMethod.prototype._mdgExecute = ValidatedMethod.prototype._execute;
 
 ValidatedMethod.prototype._execute = function _execute(...args) {
+  const userIdBeforeInvocation = CollectionHooks.defaultUserId;
   CollectionHooks.defaultUserId = args[0].userId;
-  const result = PerformanceLogger.call(this.name, this._mdgExecute, this, ...args);
-  CollectionHooks.defaultUserId = undefined;
+  let result;
+  try {
+    result = PerformanceLogger.call(this.name, this._mdgExecute, this, ...args);
+  } finally {
+    CollectionHooks.defaultUserId = userIdBeforeInvocation;
+  }
   return result;
 };
