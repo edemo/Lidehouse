50cd10e267a2c8c00fc2fdb3b1d7c7f6cbd1b68f
kmildi <keleti.ildi@gmail.com>
2022-07-21 15:06:22 +0200

    Distinguish parcelscoped permission from general in user hasPermission

--- imports/ui_3/helpers/permissions.js
+++ imports/ui_3/helpers/permissions.js
@@ -2,10 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { getActiveCommunityId } from '/imports/ui_3/lib/active-community.js';
 
-export function currentUserHasPermission(permissionName, doc) {
+export function currentUserHasPermission(permissionName, doc, notParcelScoped) {
   const user = Meteor.userOrNull();
   if (!doc || !doc._id) doc = { communityId: getActiveCommunityId() };
-  return user.hasPermission(permissionName, doc);
+  return user.hasPermission(permissionName, doc, notParcelScoped);
 }
 
 Template.registerHelper('currentUserHasPermission', currentUserHasPermission);
