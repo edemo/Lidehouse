fc569739411d049aeadb60cb773ee26274d8d24f
droka <daniel.roka@tutanota.com>
2024-01-30 14:04:09 +0100

    Fix migration

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -1228,9 +1228,12 @@ Migrations.add({
   up() {
     Communities.find({ 'settings.accountingMethod': 'cash' }).forEach(community => {
       const adminId = community.admin()._id;
+      console.log('Reposting community', community.name);
       const period = AccountingPeriods.findOne({ communityId: community._id });
       AccountingPeriods.direct.update(period._id, { $unset: { accountingClosedAt: '' } });
-      Transactions.find({ communityId: community._id, category: { $in: ['bill', 'payment'] }, status: 'posted' }).forEach(tx => {
+      const txs = Transactions.find({ communityId: community._id, category: { $in: ['bill', 'payment'] }, status: 'posted' }).fetch();
+      console.log('Tx count in community', txs.length);
+      txs.forEach(tx => {
         Transactions.methods.post._execute({ userId: adminId }, { _id: tx._id });
       });
     });
