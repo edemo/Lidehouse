45c1e32a7899cc76cffb790d3a13d089b1211389
droka <daniel.roka@tutanota.com>
2026-02-07 14:17:07 +0100

    Add account 37 to money accounts

--- imports/api/accounting/statement-entries/statement-entries.js
+++ imports/api/accounting/statement-entries/statement-entries.js
@@ -16,7 +16,7 @@ export const StatementEntries = new Mongo.Collection('statementEntries');
 
 StatementEntries.schema = new SimpleSchema({
   communityId: { type: String, regEx: SimpleSchema.RegEx.Id, autoform: { type: 'hidden' } },
-  account: { type: String, autoform: _.extend({ readonly: true }, Accounts.chooseSubNode(['`38', '`43'])) },
+  account: { type: String, autoform: _.extend({ readonly: true }, Accounts.chooseSubNode(['`37', '`38', '`43'])) },
   ref: { type: String, max: 50, optional: true }, // external (uniq) ref id provided by the bank
   refType: { type: String, max: 50, optional: true }, // type info to the ref
   time: { type: Date, optional: true, autoform: { type: 'datetime-local' } }, // http://khaidoan.wikidot.com/meteor-autoform-date
